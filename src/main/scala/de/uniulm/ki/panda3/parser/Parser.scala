39414109c5d06985ce873608eb6d7e1a561bc582
Galvus Damor <gregor.behnke@uni-ulm.de>
2015-04-09 10:51:16 +0200

    Parser Stub

--- src/main/scala/de/uniulm/ki/panda3/parser/Parser.scala
+++ src/main/scala/de/uniulm/ki/panda3/parser/Parser.scala
@@ -8,7 +8,5 @@ import de.uniulm.ki.panda3.domain.Domain
  * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
  */
 trait Parser {
-
   def parseFromFile(filename: String): Domain
-
 }
---
+++ src/main/scala/de/uniulm/ki/panda3/parser/XMLParser.scala
@@ -0,0 +1,27 @@
+package de.uniulm.ki.panda3.parser
+
+import javax.xml.bind.{JAXBContext, Unmarshaller}
+
+import de.uniulm.ki.panda3.domain.Domain
+import de.uniulm.ki.panda3.parser.xml.XMLDomain
+
+/**
+ *
+ *
+ * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
+ */
+class XMLParser extends Parser {
+
+  override def parseFromFile(filename: String): Domain = {
+    val context: JAXBContext = JAXBContext.newInstance(Domain.getClass)
+    val marshaller: Unmarshaller = context.createUnmarshaller()
+
+
+    val dom: XMLDomain = marshaller.unmarshal(new java.io.File(filename)).asInstanceOf[XMLDomain]
+
+
+    ???
+  }
+
+
+}
