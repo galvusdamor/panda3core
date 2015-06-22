979eab6fc87273acd63a104b380eb9d5ac388b9f
Galvus Damor <gregor.behnke@uni-ulm.de>
2015-06-22 18:08:10 +0200

    Added some TCs for the PANDA-XML parser

--- src/main/scala/de/uniulm/ki/panda3/parser/XMLParser.scala
+++ src/main/scala/de/uniulm/ki/panda3/parser/XMLParser.scala
@@ -17,13 +17,13 @@ import scala.collection.{JavaConversions, _}
 import scala.xml.InputSource
 
 /**
- *
+ * This is a parser for the old XML format of PANDA 1 and 2.
  *
  * @author Kadir Dede (kadir.dede@uni-ulm.de)
+ * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
  */
 object XMLParser extends Parser {
 
-  @Override
   override def parseFromFile(filename: String): Domain = {
 
     val context: JAXBContext = JAXBContext.newInstance(classOf[XMLDomain])
@@ -33,7 +33,7 @@ object XMLParser extends Parser {
     spf.setNamespaceAware(true)
     spf.setValidating(true)
     // Not required for JAXB/XInclude
-    val xr: XMLReader = spf.newSAXParser().getXMLReader()
+    val xr: XMLReader = spf.newSAXParser().getXMLReader
     val source: SAXSource = new SAXSource(xr, new InputSource(new FileInputStream(filename)))
     val dom: XMLDomain = marshaller.unmarshal(source).asInstanceOf[XMLDomain]
 
@@ -91,8 +91,8 @@ object XMLParser extends Parser {
       val splitContent = JavaConversions.asScalaBuffer(taskSchema.getContent) filter {!_.isInstanceOf[JAXBElement[Any]]} partition {_.isInstanceOf[VariableDeclaration]}
       assert(splitContent._2.size == 2)
       val xmlVariableDeclarations = splitContent._1 map {_.asInstanceOf[VariableDeclaration]}
-      val variables = xmlVariableDeclarations.zipWithIndex map { case (variableDeclaration, idx) => Variable(idx, variableDeclaration.getName, xmlSortsToScalaSorts(
-        variableDeclaration.getSort.asInstanceOf[SortDeclaration]))
+      val variables = xmlVariableDeclarations.zipWithIndex map { case (variableDeclaration, idx) => Variable(idx, variableDeclaration.getName,
+                                                                                                             xmlSortsToScalaSorts(variableDeclaration.getSort.asInstanceOf[SortDeclaration]))
       }
       val varDeclToVariable: Map[VariableDeclaration, logic.Variable] = (xmlVariableDeclarations zip variables).toMap
 
@@ -100,40 +100,38 @@ object XMLParser extends Parser {
 
       def toLiteralList(xmlStruct: Any, positive: Boolean): Seq[Literal] = {
         xmlStruct match {
-          case and: xml.And =>
+          case and: xml.And   =>
             if (positive) {
               JavaConversions.asScalaBuffer(and.getAtomicOrNotOrAnd) flatMap {toLiteralList(_, positive)}
             } else {
               assert(false)
               Nil
             }
-          case or: xml.Or =>
+          case or: xml.Or     =>
             if (!positive) {
-              JavaConversions.asScalaBuffer(or.asInstanceOf[xml.Or].getAtomicOrNotOrAnd) flatMap {toLiteralList(_, positive)}
+              JavaConversions.asScalaBuffer(or.getAtomicOrNotOrAnd) flatMap {toLiteralList(_, positive)}
             } else {
               assert(false)
               Nil
             }
-          case not: xml.Not => toLiteralList(getAnyFromNot(not), !positive)
+          case not: xml.Not   => toLiteralList(getAnyFromNot(not), !positive)
           case atomic: Atomic =>
-            val parameterVariables: scala.Seq[Variable] = JavaConversions.asScalaBuffer(atomic.getVariableOrConstant) map { case variable: xml.Variable => varDeclToVariable(
-              variable.getName.asInstanceOf[VariableDeclaration])
-            case constant: xml.Constant =>
-              val constDeclaration: ConstantDeclaration = constant.getName.asInstanceOf[ConstantDeclaration]
-              val newVariable = Variable(variables.size + variableConstraints.size, "ConstantVariable" + constant.getName,
-                                         xmlSortsToScalaSorts(constDeclaration.getSort.asInstanceOf[SortDeclaration]))
-              variableConstraints = variableConstraints :+ Equal(newVariable, xmlConstantToScalaConstant(constDeclaration))
-              newVariable
+            val parameterVariables: scala.Seq[Variable] = JavaConversions.asScalaBuffer(atomic.getVariableOrConstant) map {
+              case variable: xml.Variable => varDeclToVariable(variable.getName.asInstanceOf[VariableDeclaration])
+              case constant: xml.Constant =>
+                val constDeclaration: ConstantDeclaration = constant.getName.asInstanceOf[ConstantDeclaration]
+                val newVariable = Variable(variables.size + variableConstraints.size, "ConstantVariable" + constant.getName,
+                                           xmlSortsToScalaSorts(constDeclaration.getSort.asInstanceOf[SortDeclaration]))
+                variableConstraints = variableConstraints :+ Equal(newVariable, xmlConstantToScalaConstant(constDeclaration))
+                newVariable
             }
             Literal(xmlPredicateToScalaPredicate(atomic.getRelation.asInstanceOf[RelationDeclaration]), positive, parameterVariables) :: Nil
-          case _ => Nil
+          case _              => Nil
         }
-        // TODO: handle exisistential quantifier
+        // TODO: handle existential quantifier
       }
-
       val preconditions = toLiteralList(splitContent._2(0), positive = true)
       val effects = toLiteralList(splitContent._2(1), positive = true)
-      // TODO: add constant variables
       Task(taskSchema.getName, taskSchema.getType == "primitive", variables ++ (variableConstraints map { case Equal(vari, _) => vari }), variableConstraints, preconditions, effects)
     }
 
@@ -147,11 +145,9 @@ object XMLParser extends Parser {
     // Sequence of Decomposition Methods
     // Sequence of Decomposition Axioms
 
-    Domain(sortSeq, predicates, Nil, Nil, Nil)
+    Domain(sortSeq, predicates, tasks, Nil, Nil)
   }
 
 
   private def getAnyFromNot(not: xml.Not): Any = ((not.getAnd :: not.getAtomic :: not.getExists :: not.getForall :: not.getImply :: not.getNot :: not.getOr :: Nil) find {_ != null}).get
-
-
 }
--- src/test/scala/de/uniulm/ki/panda3/parser/XMLParserTest.scala
+++ src/test/scala/de/uniulm/ki/panda3/parser/XMLParserTest.scala
@@ -1,22 +1,88 @@
 package de.uniulm.ki.panda3.parser
 
 import de.uniulm.ki.panda3.domain.Domain
+import de.uniulm.ki.panda3.logic.Literal
 import org.scalatest.FlatSpec
 
 /**
- *
- *
  * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
  */
 class XMLParserTest extends FlatSpec {
 
 
-  "Parsing Files " must "be possible without error" in {
+  "Parsing hon-hierarchical Files " must "be possible without error" in {
     val dom: Domain = XMLParser.parseFromFile("src/test/resources/de/uniulm/ki/panda3/parser/AssemblyTask_domain.xml")
 
     assert(dom.constants.size == 0)
     assert(dom.sorts.size == 34)
+    assert(dom.predicates.size == 12)
+    assert(dom.tasks.size == 16)
+
+    // checking one sort
+    val cableAudioAndVideoSortOpt = dom.sorts find {_.name == "Cable_Audio_and_Video"}
+    assert(cableAudioAndVideoSortOpt.isDefined)
+    val cableAudioAndVideoSort = cableAudioAndVideoSortOpt.get
+
+    assert(cableAudioAndVideoSort.elements.size == 0)
+    assert(cableAudioAndVideoSort.subSorts.size == 2)
+    assert(cableAudioAndVideoSort.subSorts forall { s => s.name == "Cable_HDMI" || s.name == "Cable_Scart_CinchVideo_CinchStereo" })
   }
 
+  "Parsing hierarchical Files " must "be possible without error" in {
+    val dom: Domain = XMLParser.parseFromFile("src/test/resources/de/uniulm/ki/panda3/parser/SmartPhone-HierarchicalNoAxioms.xml")
 
+    assert(dom.constants.size == 0)
+    assert(dom.sorts.size == 40)
+    assert(dom.predicates.size == 63)
+    assert(dom.tasks.size == 138)
+
+    assert((dom.tasks count {_.isPrimitive}) == 88)
+    assert((dom.tasks count {!_.isPrimitive}) == 50)
+
+    // checking some tasks
+    // a grounded one
+    val task1Opt = dom.tasks find {_.name == "press_People.Smallplus_AlreadyPressed"}
+    assert(task1Opt.isDefined)
+    val task1 = task1Opt.get
+    assert(task1.parameters.size == 0)
+    assert(task1.isPrimitive)
+    assert(task1.parameterConstraints.size == 0)
+    assert(task1.preconditions.size == 3)
+    assert(task1.effects.size == 2)
+
+    assert(task1.preconditions exists { case Literal(predicate, isPositive, parameter) => predicate.name == "inMode_People" && isPositive && parameter.size == 0 })
+    assert(task1.preconditions exists { case Literal(predicate, isPositive, parameter) => predicate.name == "inMode_People.ReadyToAddFavourite" && isPositive && parameter.size == 0 })
+    assert(task1.preconditions exists { case Literal(predicate, isPositive, parameter) => predicate.name == "inMode_Menu" && !isPositive && parameter.size == 0 })
+    assert(task1.effects exists { case Literal(predicate, isPositive, parameter) => predicate.name == "inMode_People.ReadyToAddFavourite" && isPositive && parameter.size == 0 })
+    assert(task1.effects exists { case Literal(predicate, isPositive, parameter) => predicate.name == "inMode_People.FavouriteSelected" && !isPositive && parameter.size == 0 })
+
+    // a lifted one
+    val task2Opt = dom.tasks find {_.name == "select_Contacts.ContactForContactable"}
+    assert(task2Opt.isDefined)
+    val task2 = task2Opt.get
+    assert(task2.parameters.size == 2)
+    assert(task2.isPrimitive)
+    assert(task2.parameterConstraints.size == 0)
+    assert(task2.preconditions.size == 4)
+    assert(task2.effects.size == 2)
+
+
+    val variable1Opt = task2.parameters find { v => v.name == "newObj_variable78" && v.sort.name == "Contact" && dom.sorts.contains(v.sort) }
+    assert(variable1Opt.isDefined)
+    val variable1 = variable1Opt.get
+    val variable2Opt = task2.parameters find { v => v.name == "newObj_variable79" && v.sort.name == "Contactable" && dom.sorts.contains(v.sort) }
+    assert(variable2Opt.isDefined)
+    val variable2 = variable2Opt.get
+
+    assert(task2.preconditions exists { case Literal(predicate, isPositive, parameter) => predicate.name == "inMode_Contacts" && isPositive && parameter.size == 0 })
+    assert(task2.preconditions exists { case Literal(predicate, isPositive, parameter) => predicate.name == "inMode_Contacts.ConfigureContact" && !isPositive && parameter.size == 0 })
+    assert(task2.preconditions exists { case Literal(predicate, isPositive, parameter) => predicate.name == "inMode_Contacts.Details" && !isPositive && parameter.size == 0 })
+    assert(task2.preconditions exists { case Literal(predicate, isPositive, parameter) => predicate.name == "associated_Contact" && isPositive && parameter.size == 2 &&
+      parameter(0) == variable1 && parameter(1) == variable2
+    })
+
+    assert(task2.effects exists { case Literal(predicate, isPositive, parameter) => predicate.name == "inMode_Contacts.Details" && isPositive && parameter.size == 0 })
+    assert(task2.effects exists { case Literal(predicate, isPositive, parameter) => predicate.name == "selected" && isPositive && parameter.size == 1 && parameter.head == variable1 })
+
+  }
 }
