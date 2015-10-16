27deb6d94f885fdf19dfe92d2c0292cd68837e25
Galvus Damor <gregor.behnke@uni-ulm.de>
2015-10-16 14:45:40 +0200

    Added an explicit representation of the search tree
    * added the classes for the representation
    * changed the interface of the DFS search prodruces s.t. it produces a search tree

--- src/main/scala/de/uniulm/ki/panda3/symbolic/search/DFS.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/search/DFS.scala
@@ -1,5 +1,7 @@
 package de.uniulm.ki.panda3.symbolic.search
 
+import java.util.concurrent.Semaphore
+
 import de.uniulm.ki.panda3.symbolic.compiler.ClosedWorldAssumption
 import de.uniulm.ki.panda3.symbolic.domain.Domain
 import de.uniulm.ki.panda3.symbolic.parser.XMLParser
@@ -19,57 +21,69 @@ object DFS {
     val sortExpansion = domAndInitialPlan._1.expandSortHierarchy()
 
     val parsedDom = domAndInitialPlan._1.update(sortExpansion)
-    val paredProblem = domAndInitialPlan._2.update(sortExpansion)
+    val parsedProblem = domAndInitialPlan._2.update(sortExpansion)
 
     // apply the CWA
-    val cwaApplied = ClosedWorldAssumption.transform(parsedDom, paredProblem, ())
+    val cwaApplied = ClosedWorldAssumption.transform(parsedDom, parsedProblem, ())
 
-    initTime = System.currentTimeMillis()
-    println(search(cwaApplied._1, cwaApplied._2))
+    val x = startSearch(cwaApplied._1, cwaApplied._2)
+
+    x._2.acquire()
+    println("100 Nodes generated")
   }
 
-  var nodes: Int = 0
-  var initTime: Long = 0
-  var d: Int = 0
-  var crap: Int = 0
+  /**
+   * This functions starts the (asynchronious) search for a solution to a given planning problem.
+   * It returns a pointer to the search space and a semaphore signaling that new search nodes have been inserted into the tree.
+   *
+   * The semaphore will not be released for every search node, but after a couple 100 ones.
+   */
+  def startSearch(domain: Domain, initialPlan: Plan): (SearchNode, Semaphore) = {
+    val semaphore: Semaphore = new Semaphore(0)
+    val node: SearchNode = new SearchNode(initialPlan, null, -1)
 
-  def search(domain: Domain, plan: Plan): Option[Plan] = if (plan.flaws.size == 0) Some(plan)
-  else {
-    nodes = nodes + 1
-    d = d + 1
-    if (nodes % 10 == 0) println(nodes * 1000.0 / (System.currentTimeMillis() - initTime) + " " + d + s" $crap/$nodes")
-    //println()
-    //println("CALL with Plan:")
-    //println("---------------")
-    //println(plan.shortInfo)
-    var then = System.currentTimeMillis()
+    // variables for the search
+    val initTime: Long = System.currentTimeMillis()
+    var nodes: Int = 0 // count the nodes
+    var d: Int = 0 // the depth
+    var crap: Int = 0 // and how many dead ends we have encountered
 
-    val flaws = plan.flaws
-    val resolvers = flaws map {_.resolvents(domain)}
-
-    var now = System.currentTimeMillis()
-    //println("Time to calculate resolvers: " + (now - then))
-
-    //println()
-    //println("Flaws " + ((resolvers zip flaws) map {case (rs,f) => rs.size + " " + f.shortInfo}).mkString("\n"))
-
-    if (resolvers exists {_.size == 0}) {d = d - 1; crap = crap + 1; None}
+    def search(domain: Domain, node: SearchNode): Option[Plan] = if (node.plan.flaws.size == 0) Some(node.plan)
     else {
-      val selectedResolvers: Seq[Modification] = (resolvers filterNot {_.size == 0}).head
-      //println(selectedResolvers.size)
+      nodes = nodes + 1
+      d = d + 1
+      if (nodes % 10 == 0) println(nodes * 1000.0 / (System.currentTimeMillis() - initTime) + " " + d + s" $crap/$nodes")
+      if (nodes % 100 == 0) semaphore.release()
 
-      val ret = selectedResolvers.foldLeft[Option[Plan]](None)({
-        case (Some(p), _) => Some(p)
-        case (None, res)  =>
-          then = System.currentTimeMillis()
-          val nextPlan = plan.modify(res)
-          now = System.currentTimeMillis()
-          //println("Time to apply modification: " + (now - then))
-          search(domain, nextPlan)
-      })
-      d = d - 1
+      val flaws = node.plan.flaws
+      node.modifications = flaws map {_.resolvents(domain)}
 
-      ret
+      // check whether we are at a dead end in the search space
+      if (node.modifications exists {_.size == 0}) {d = d - 1; crap = crap + 1; node.dirty = false; None}
+      else {
+        val selectedResolvers: Seq[Modification] = (node.modifications filterNot {_.size == 0}).head
+        // set the selected flaw
+        node.selectedFlaw = node.modifications indexOf selectedResolvers
+
+        // create all children
+        node.children = selectedResolvers map { m => new SearchNode(node.plan.modify(m), node, -1) }
+        node.dirty = false
+
+        // perform the search
+        val ret = node.children.foldLeft[Option[Plan]](None)({
+          case (Some(p), _) => Some(p)
+          case (None, res)  => search(domain, res)
+        })
+        d = d - 1
+
+        ret
+      }
     }
+
+    new Thread(new Runnable {
+      override def run(): Unit = search(domain, node)
+    }).start()
+
+    (node, semaphore)
   }
 }
---
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/search/SearchNode.scala
@@ -0,0 +1,31 @@
+package de.uniulm.ki.panda3.symbolic.search
+
+import de.uniulm.ki.panda3.symbolic.plan.Plan
+import de.uniulm.ki.panda3.symbolic.plan.modification.Modification
+
+/**
+ * Represents a search state in a search space. This structure will (usually, i.e., we will probably never have loops do the systematicity) be a tree.
+ *
+ * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
+ */
+class SearchNode(nodePlan: Plan, nodeParent: SearchNode, nodeHeuristic: Double) {
+  /** the plan contained in this search node, if it has no flaws this is a solution */
+  val plan: Plan = nodePlan
+  /** the nodes parent */
+  val parent: SearchNode = nodeParent
+  /** the computed heuristic of this node. This might be -1 if the search procedure does not use a heuristic */
+  val heuristic: Double = nodeHeuristic
+
+  /** if this flag is true only the current plan, the heuristic and its parent are valid! Do not read any other information */
+  var dirty: Boolean = true
+
+
+  /** the flaw selected for refinement */
+  var selectedFlaw: Int = -1
+  /** the possible modifications for all flaws. If one of the lists is empty this is a dead-end node in the search space */
+  var modifications: Seq[Seq[Modification]] = Nil
+  /** the successors based on the list of modifications */
+  var children: Seq[SearchNode] = Nil
+  /** any possible further payload */
+  var payload: Any = null
+}
