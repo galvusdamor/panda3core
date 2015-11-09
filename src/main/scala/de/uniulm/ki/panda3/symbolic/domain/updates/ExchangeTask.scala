f8c5bd488efeb5f6a8c26d040e2d18b73cf962b3
Galvus Damor <gregor.behnke@uni-ulm.de>
2015-11-09 15:13:54 +0100

    Another update

---
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/domain/updates/ExchangeTask.scala
@@ -0,0 +1,12 @@
+package de.uniulm.ki.panda3.symbolic.domain.updates
+
+import de.uniulm.ki.panda3.symbolic.domain.Task
+
+/**
+ *
+ *
+ * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
+ */
+case class ExchangeTask(exchange: Map[Task, Task]) {
+
+}
