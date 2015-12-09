29b314957a8c3798ef288765e96021b02c2719aa
Galvus Damor <gregor.behnke@uni-ulm.de>
2015-12-09 19:47:50 +0100

    Fixed a compile error ...

--- src/main/scala/de/uniulm/ki/panda3/efficient/plan/EfficientPlan.scala
+++ src/main/scala/de/uniulm/ki/panda3/efficient/plan/EfficientPlan.scala
@@ -5,6 +5,6 @@ package de.uniulm.ki.panda3.efficient.plan
  *
  * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
  */
-case class EfficientPlan(planSteps: Array[Int], planStepParameters: Array[Array[Int]], ) {
+case class EfficientPlan(planSteps: Array[Int], planStepParameters: Array[Array[Int]]) {
 
 }
