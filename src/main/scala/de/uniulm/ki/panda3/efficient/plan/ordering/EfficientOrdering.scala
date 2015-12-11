2bf44c868551ebe4707486677e2f113bf489ff4e
Galvus Damor <gregor.behnke@uni-ulm.de>
2015-12-11 18:48:46 +0100

    Added and tested an implementation of an efficient task ordering

---
+++ src/main/scala/de/uniulm/ki/panda3/efficient/plan/ordering/EfficientOrdering.scala
@@ -0,0 +1,148 @@
+package de.uniulm.ki.panda3.efficient.plan.ordering
+
+import de.uniulm.ki.panda3.symbolic.plan.ordering.TaskOrdering._
+
+/**
+ * The assumption is, that there are exactly sz(orderingConstraints) many tasks, which are numbered 0..sz(orderingConstraints)-1
+ *
+ * The matrix contained in this object describes the relation between two tasks using the constants of the companion object [[de.uniulm.ki.panda3.symbolic.plan.ordering.TaskOrdering]]
+ *
+ * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
+ */
+class EfficientOrdering(val orderingConstraints: Array[Array[Byte]] = Array(), var isConsistent: Boolean = true) extends PartialOrdering[Int] {
+
+  /**
+   * Propagate newly inserted ordering constraints using Bellman-Ford
+   */
+  private def propagate(edgesFrom: Array[Int], edgesTo: Array[Int]): Unit = {
+    assert(edgesFrom.length == edgesTo.length)
+    var edge = 0
+    while (edge < edgesFrom.length && isConsistent) {
+      var from = 0
+      val ordEdgeFromEdgeTo = orderingConstraints(edgesFrom(edge))(edgesTo(edge))
+      while (from < orderingConstraints.length && isConsistent) {
+        val ordFromEdgeFrom = orderingConstraints(from)(edgesFrom(edge))
+        if (ordFromEdgeFrom != DONTKNOW) {
+          var to = 0
+          while (to < orderingConstraints.length && isConsistent) {
+            // check whether from -> edgesFrom[i] -> edgesTo[i] -> to entails something
+            val ordEdgeToTo = orderingConstraints(edgesTo(edge))(to)
+            if (ordEdgeToTo != DONTKNOW) {
+              // check whether "before" is implied
+              if (ordFromEdgeFrom <= SAME && ordEdgeFromEdgeTo <= SAME && ordEdgeToTo <= SAME) {
+                val inferredOrdering = math.min(ordFromEdgeFrom, math.min(ordEdgeFromEdgeTo, ordEdgeToTo))
+                if (orderingConstraints(from)(to) == DONTKNOW || orderingConstraints(from)(to) == inferredOrdering) orderingConstraints(from)(to) = inferredOrdering.toByte
+                else isConsistent = false
+              }
+              // check whether "after" is implied
+              if (ordFromEdgeFrom >= SAME && ordEdgeFromEdgeTo >= SAME && ordEdgeToTo >= SAME) {
+                val inferredOrdering = math.max(ordFromEdgeFrom, math.max(ordEdgeFromEdgeTo, ordEdgeToTo))
+                if (orderingConstraints(from)(to) == DONTKNOW || orderingConstraints(from)(to) == inferredOrdering) orderingConstraints(from)(to) = inferredOrdering.toByte
+                else isConsistent = false
+              }
+            }
+            to = to + 1
+          }
+        }
+        from = from + 1
+      }
+      edge = edge + 1
+    }
+  }
+
+  override def tryCompare(x: Int, y: Int): Option[Int] =
+    if (x >= orderingConstraints.length || y >= orderingConstraints.length) None
+    else if (orderingConstraints(x)(y) == DONTKNOW) None else Some(orderingConstraints(x)(y))
+
+  override def lteq(x: Int, y: Int): Boolean = if (x >= orderingConstraints.length || y >= orderingConstraints.length) false else orderingConstraints(x)(y) <= SAME
+
+
+  def copy(): EfficientOrdering = addPlanSteps(0)
+
+  def addPlanSteps(newPlanSteps: Int): EfficientOrdering = addPlanStepsWithMaybeFromBase(fromBase = false, -1, null, -1, newPlanSteps)
+
+  def addOrderingConstraint(before: Int, after: Int): Unit =
+    if (orderingConstraints(before)(after) == SAME || orderingConstraints(before)(after) == AFTER) isConsistent = false
+    else {
+      orderingConstraints(before)(after) = BEFORE
+      orderingConstraints(after)(before) = AFTER
+      propagate(Array(before, after), Array(after, before))
+    }
+
+  def removePlanStep(ps: Int): EfficientOrdering = {
+    val newOrdering = new Array[Array[Byte]](orderingConstraints.length - 1)
+    var i = 0
+    while (i < newOrdering.length) {
+      newOrdering(i) = new Array[Byte](newOrdering.length)
+      var j = 0
+      while (j < newOrdering.length) {
+        newOrdering(i)(j) = orderingConstraints(i + (if (i >= ps) 1 else 0))(j + (if (j >= ps) 1 else 0))
+        j = j + 1
+      }
+      i = i + 1
+    }
+    new EfficientOrdering(newOrdering, isConsistent)
+  }
+
+
+  /**
+   * indexBaseInNew will assume the index of oldPlanStep, while the newly added plan steps will receive the next sz(internalOrdering)-1 numbers
+   */
+  def replacePlanStep(oldPlanStep: Int, ordering: EfficientOrdering, indexBaseInNew: Int): EfficientOrdering =
+    addPlanStepsWithMaybeFromBase(fromBase = true, oldPlanStep, ordering.orderingConstraints, indexBaseInNew, -1)
+
+
+  private def addPlanStepsWithMaybeFromBase(fromBase: Boolean, base: Int, internalOrdering: Array[Array[Byte]], indexBaseInNew: Int, newPlanSteps: Int): EfficientOrdering = {
+    val originalSize = orderingConstraints.length
+    val newOrdering = new Array[Array[Byte]](originalSize + (if (fromBase) internalOrdering.length - 1 else newPlanSteps))
+    var i = 0
+    while (i < originalSize) {
+      newOrdering(i) = new Array[Byte](newOrdering.length)
+      var j = 0
+      while (j < originalSize) {
+        // copy the original matrix
+        newOrdering(i)(j) = orderingConstraints(i)(j)
+        j = j + 1
+      }
+      while (j < newOrdering.length) {
+        // copy the original matrix
+        if (fromBase) {
+          if (i != base) newOrdering(i)(j) = orderingConstraints(i)(base)
+          else {
+            var indexOnNewOrdering = j - originalSize
+            if (indexOnNewOrdering >= indexBaseInNew) indexOnNewOrdering = indexOnNewOrdering + 1
+            newOrdering(i)(j) = internalOrdering(indexBaseInNew)(indexOnNewOrdering)
+          }
+        } else newOrdering(i)(j) = DONTKNOW
+        j = j + 1
+      }
+      i = i + 1
+    }
+    while (i < newOrdering.length) {
+      newOrdering(i) = new Array[Byte](newOrdering.length)
+      var j = 0
+      while (j < newOrdering.length) {
+        if (i == j) newOrdering(i)(j) = SAME
+        else if (!fromBase) newOrdering(i)(j) = DONTKNOW
+        else if (j < originalSize) {
+          if (j != base) newOrdering(i)(j) = orderingConstraints(base)(j)
+          else {
+            var indexOnNewOrdering = i - originalSize
+            if (indexOnNewOrdering >= indexBaseInNew) indexOnNewOrdering = indexOnNewOrdering + 1
+            newOrdering(i)(j) = internalOrdering(indexOnNewOrdering)(indexBaseInNew)
+          }
+        } else {
+          var iIndexOnNewOrdering = i - originalSize
+          if (iIndexOnNewOrdering >= indexBaseInNew) iIndexOnNewOrdering = iIndexOnNewOrdering + 1
+          var jIndexOnNewOrdering = j - originalSize
+          if (jIndexOnNewOrdering >= indexBaseInNew) jIndexOnNewOrdering = jIndexOnNewOrdering + 1
+          // apply
+          newOrdering(i)(j) = internalOrdering(iIndexOnNewOrdering)(jIndexOnNewOrdering)
+        }
+        j = j + 1
+      }
+      i = i + 1
+    }
+    new EfficientOrdering(newOrdering, isConsistent)
+  }
+}
---
+++ src/test/scala/de/uniulm/ki/panda3/efficient/plan/ordering/EfficientOrderingTest.scala
@@ -0,0 +1,217 @@
+package de.uniulm.ki.panda3.efficient.plan.ordering
+
+import org.scalatest.FlatSpec
+
+/**
+ *
+ *
+ * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
+ */
+class EfficientOrderingTest extends FlatSpec {
+
+  "Ordering inference" must "allow trivial inference" in {
+    val ordering = new EfficientOrdering().addPlanSteps(3)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(0, 1)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(1, 2)
+    assert(ordering.isConsistent)
+
+    for (i <- Range(0, 3)) assert(ordering.equiv(i, i))
+    assert(ordering.lt(0, 2))
+    assert(ordering.lt(0, 1))
+    assert(ordering.lt(1, 2))
+  }
+
+  it must "allow almost trivial inference" in {
+    val ordering = new EfficientOrdering().addPlanSteps(9)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(0, 1)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(1, 2)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(2, 3)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(3, 4)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(3, 5)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(6, 2)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(2, 7)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(7, 3)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(2, 8)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(8, 3)
+    assert(ordering.isConsistent)
+
+
+    assert(ordering.lteq(0, 2))
+    assert(ordering.lteq(0, 3))
+    assert(ordering.lteq(0, 4))
+    assert(ordering.lteq(0, 5))
+    assert(ordering.lteq(6, 4))
+    assert(ordering.lteq(6, 7))
+    assert(!ordering.lteq(4, 5))
+    assert(!ordering.lteq(5, 1))
+    assert(ordering.tryCompare(6, 1) === None)
+    assert(ordering.tryCompare(5, 7) === Some(1))
+    assert(ordering.tryCompare(6, 4) === Some(-1))
+    assert(ordering.tryCompare(7, 8) === None)
+  }
+
+  "Orderings inconsistencies" must "find simple inconsistencies" in {
+    // a dummy plan
+    val order = new EfficientOrdering().addPlanSteps(3)
+    order.addOrderingConstraint(0, 1)
+    order.addOrderingConstraint(1, 2)
+    order.addOrderingConstraint(2, 0)
+
+    assert(!order.isConsistent)
+  }
+
+  it must "find tricky inconsistencies" in {
+    // a dummy plan
+    val order = new EfficientOrdering().addPlanSteps(2)
+    order.addOrderingConstraint(0, 1)
+    order.addOrderingConstraint(1, 0)
+
+    assert(!order.isConsistent)
+  }
+
+  it must "find complex inconsistencies" in {
+    val ordering = new EfficientOrdering().addPlanSteps(8)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(0, 1)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(1, 2)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(2, 3)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(3, 4)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(3, 5)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(6, 2)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(2, 7)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(7, 3)
+    assert(ordering.isConsistent)
+    ordering.addOrderingConstraint(5, 1)
+
+    assert(!ordering.isConsistent)
+  }
+
+  "Removing plan steps" must "lead to a correct reamining order" in {
+    val ordering = new EfficientOrdering().addPlanSteps(5)
+    ordering.addOrderingConstraint(0,1)
+    ordering.addOrderingConstraint(1,2)
+    ordering.addOrderingConstraint(2,3)
+    ordering.addOrderingConstraint(0,4)
+    ordering.addOrderingConstraint(4,3)
+    assert(ordering.isConsistent)
+
+    // test whether the ordering looks like we expect it
+    assert(ordering.lt(0,1))
+    assert(ordering.lt(0,2))
+    assert(ordering.lt(0,3))
+    assert(ordering.lt(1,2))
+    assert(ordering.lt(1,3))
+    assert(ordering.lt(2,3))
+
+    assert(ordering.lt(0,4))
+    assert(ordering.lt(4,3))
+    assert(ordering.tryCompare(1,4) === None)
+    assert(ordering.tryCompare(2,4) === None)
+
+    // remove 2
+    val removedOrdering = ordering.removePlanStep(2)
+    assert(removedOrdering.isConsistent)
+    // test whether the ordering looks like we expect it
+    assert(removedOrdering.lt(0,1))
+    assert(removedOrdering.lt(0,2))
+    assert(removedOrdering.lt(1,2))
+
+    assert(removedOrdering.lt(0,3))
+    assert(removedOrdering.lt(3,2))
+    assert(removedOrdering.tryCompare(1,3) === None)
+  }
+
+  "Adding a subordering replacing a single task" must "be correct" in {
+    val mainOrdering = new EfficientOrdering().addPlanSteps(5)
+    mainOrdering.addOrderingConstraint(0,1)
+    mainOrdering.addOrderingConstraint(1,2)
+    mainOrdering.addOrderingConstraint(0,3)
+    mainOrdering.addOrderingConstraint(3,2)
+    mainOrdering.addOrderingConstraint(2,4)
+    assert(mainOrdering.isConsistent)
+    assert(mainOrdering.lt(0,2))
+    assert(mainOrdering.lt(0,4))
+    assert(mainOrdering.lt(1,4))
+    assert(mainOrdering.lt(3,4))
+
+    val subOrdering = new EfficientOrdering().addPlanSteps(5)
+    subOrdering.addOrderingConstraint(0,2)
+    subOrdering.addOrderingConstraint(1,2)
+    subOrdering.addOrderingConstraint(2,3)
+
+    // replace 1 in the main ordering with the subordering and let 2 be the representative
+    val replaced = mainOrdering.replacePlanStep(1,subOrdering,2)
+    assert(replaced.isConsistent)
+    assert(replaced.orderingConstraints.length == 9)
+
+    // direct relations
+    assert(replaced.lt(0,5))
+    assert(replaced.lt(0,6))
+    assert(replaced.lt(0,8))
+    assert(replaced.lt(5,1))
+    assert(replaced.lt(6,1))
+    assert(replaced.lt(1,7))
+    assert(replaced.lt(7,2))
+    assert(replaced.lt(8,2))
+    assert(replaced.lt(0,3))
+    assert(replaced.lt(0,2))
+
+    // dome impled ones
+    assert(replaced.lt(5,4))
+    assert(replaced.lt(6,4))
+    assert(replaced.lt(8,4))
+    assert(replaced.lt(1,4))
+    assert(replaced.lt(7,4))
+
+    // uncomparable external
+    assert(replaced.tryCompare(1,3) === None)
+    assert(replaced.tryCompare(5,3) === None)
+    assert(replaced.tryCompare(6,3) === None)
+    assert(replaced.tryCompare(8,3) === None)
+    assert(replaced.tryCompare(7,3) === None)
+    // uncomparable internal
+    assert(replaced.tryCompare(8,5) === None)
+    assert(replaced.tryCompare(8,6) === None)
+    assert(replaced.tryCompare(8,1) === None)
+    assert(replaced.tryCompare(8,7) === None)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
