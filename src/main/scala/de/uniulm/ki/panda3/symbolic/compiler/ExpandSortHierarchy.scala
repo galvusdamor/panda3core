c75e41365419cba5b5f4dbdc10d8315d6ac0e40a
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-01-22 20:06:24 +0100

    Added a compiler wrapper expanding the sort hierarchy

---
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/compiler/ExpandSortHierarchy.scala
@@ -0,0 +1,17 @@
+package de.uniulm.ki.panda3.symbolic.compiler
+
+import de.uniulm.ki.panda3.symbolic.domain.Domain
+import de.uniulm.ki.panda3.symbolic.plan.Plan
+
+/**
+  * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
+  */
+object ExpandSortHierarchy extends DomainTransformer[Unit]{
+
+  /** expands the sort hierarchy */
+  override def transform(domain: Domain, plan: Plan, info: Unit): (Domain, Plan) = {
+    val update = domain.expandSortHierarchy()
+
+    (domain update update, plan update update)
+  }
+}
