b570df722315fd9dd5b6843d8e78557324c4015a
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-01-28 20:43:59 +0100

    A last one

--- src/main/scala/de/uniulm/ki/panda3/symbolic/logic/Constant.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/logic/Constant.scala
@@ -2,13 +2,14 @@ package de.uniulm.ki.panda3.symbolic.logic
 
 import de.uniulm.ki.panda3.symbolic.PrettyPrintable
 import de.uniulm.ki.panda3.symbolic.domain.updates.DomainUpdate
+import de.uniulm.ki.util.HashMemo
 
 /**
  * An object of first order logic we use to represented states.
  *
  * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
  */
-case class Constant(name: String) extends Value with PrettyPrintable {
+case class Constant(name: String) extends Value with PrettyPrintable with HashMemo{
   override def update(domainUpdate: DomainUpdate): Constant = this
 
   /** returns a short information about the object */
