3614c9c9b55702bff2b9099e8edb01025af911e2
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-01-29 17:12:58 +0100

    The DFS procedure now actually prints the solution

--- src/main/scala/de/uniulm/ki/panda3/symbolic/search/DFS.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/search/DFS.scala
@@ -94,7 +94,7 @@ object DFS {
     }
 
     new Thread(new Runnable {
-      override def run(): Unit = search(domain, node)
+      override def run(): Unit = println(search(domain, node))
     }).start()
 
     (node, semaphore, {_ => abort = true})
