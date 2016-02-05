0bbc7804d2647a7c9deecf018836e2ece17d3bf2
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-02-05 16:06:44 +0100

    added apply methods to the bimap

--- src/main/scala/de/uniulm/ki/util/BiMap.scala
+++ src/main/scala/de/uniulm/ki/util/BiMap.scala
@@ -9,6 +9,11 @@ case class BiMap[A, B](toMap: Map[A, B], fromMap: Map[B, A]) {
   // consistency
   fromMap foreach { case (a, b) => assert(toMap(b) == a) }
   toMap foreach { case (a, b) => assert(fromMap(b) == a) }
+
+
+  def apply(a: A): B = toMap(a)
+
+  def apply(b: B): A = fromMap(b)
 }
 
 object BiMap {
@@ -16,6 +21,6 @@ object BiMap {
     val toMap = pairs.toMap
     val fromMap = (pairs map { _.swap }).toMap
 
-    BiMap(toMap,fromMap)
+    BiMap(toMap, fromMap)
   }
 }
