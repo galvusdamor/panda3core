e94259924f9665a74629db80a8117de8d8968928
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-02-26 20:26:44 +0100

    Integrated efficient decomposition into the planner
    - as of now the implementation has not been validated

--- src/main/scala/de/uniulm/ki/panda3/efficient/plan/flaw/EfficientAbstractPlanStep.scala
+++ src/main/scala/de/uniulm/ki/panda3/efficient/plan/flaw/EfficientAbstractPlanStep.scala
@@ -1,11 +1,11 @@
 package de.uniulm.ki.panda3.efficient.plan.flaw
 
 import de.uniulm.ki.panda3.efficient.plan.EfficientPlan
-import de.uniulm.ki.panda3.efficient.plan.modification.EfficientModification
+import de.uniulm.ki.panda3.efficient.plan.modification.{EfficientDecomposePlanStep, EfficientModification}
 
 /**
   * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
   */
 case class EfficientAbstractPlanStep(plan: EfficientPlan, planStep: Int) extends EfficientFlaw {
-  override lazy val resolver: Array[EfficientModification] = ???
+  override lazy val resolver: Array[EfficientModification] = EfficientDecomposePlanStep(plan, this, planStep)
 }
--- src/main/scala/de/uniulm/ki/panda3/efficient/plan/flaw/EfficientOpenPrecondition.scala
+++ src/main/scala/de/uniulm/ki/panda3/efficient/plan/flaw/EfficientOpenPrecondition.scala
@@ -2,7 +2,8 @@ package de.uniulm.ki.panda3.efficient.plan.flaw
 
 import de.uniulm.ki.panda3.efficient.csp.EfficientVariableConstraint
 import de.uniulm.ki.panda3.efficient.plan.EfficientPlan
-import de.uniulm.ki.panda3.efficient.plan.modification.{EfficientInsertPlanStepWithLink, EfficientInsertCausalLink, EfficientModification}
+import de.uniulm.ki.panda3.efficient.plan.modification.{EfficientDecomposePlanStep, EfficientInsertPlanStepWithLink, EfficientInsertCausalLink, EfficientModification}
+import de.uniulm.ki.util._
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -19,12 +20,19 @@ case class EfficientOpenPrecondition(plan: EfficientPlan, planStep: Int, precond
     val buffer = new ArrayBuffer[EfficientModification]()
     buffer appendAll EfficientInsertCausalLink(plan, this, planStep, preconditionIndex)
     buffer appendAll EfficientInsertPlanStepWithLink(plan, this, planStep, preconditionIndex)
+
+    // TODO decompose only those plan steps that can lead to the necessary effect
+    var possibleProducer = 2
+    while (possibleProducer < plan.firstFreePlanStepID) {
+      if (!plan.domain.tasks(plan.planStepTasks(possibleProducer)).isPrimitive && possibleProducer != planStep) buffer appendAll EfficientDecomposePlanStep(plan, this, possibleProducer)
+      possibleProducer += 1
+    }
     buffer.toArray
   }
 
 
   /** the number of tasks (newTasks) follows the assumption that the last newTask tasks of the plan are new */
-  def updateToNewPlan(newPlan: EfficientPlan, newTasks: Int): EfficientOpenPrecondition = {
+  def updateToNewPlan(newPlan: EfficientPlan, newTasks: Int, decomposedPlanSteps: Array[Int]): EfficientOpenPrecondition = {
     val newResolvers = new ArrayBuffer[EfficientModification]()
     val flaw = EfficientOpenPrecondition(newPlan, this.planStep, this.preconditionIndex)
     // copy the old ones
@@ -32,9 +40,12 @@ case class EfficientOpenPrecondition(plan: EfficientPlan, planStep: Int, precond
     while (i < resolver.length) {
       if (resolver(i).isInstanceOf[EfficientInsertCausalLink]) {
         val asInsertLink = resolver(i).asInstanceOf[EfficientInsertCausalLink]
-        val newLink = asInsertLink.causalLink.addOffsetToPlanStepsIfGreaterThan(newTasks, plan.firstFreePlanStepID)
-        newResolvers append EfficientInsertCausalLink(newPlan, flaw, newLink, asInsertLink.necessaryVariableConstraints)
-      } else {
+        // if the planstep was decomposed the link cannot be inserted any more
+        if (!arrayContains(decomposedPlanSteps, asInsertLink.causalLink.producer)) {
+          val newLink = asInsertLink.causalLink.addOffsetToPlanStepsIfGreaterThan(newTasks, plan.firstFreePlanStepID)
+          newResolvers append EfficientInsertCausalLink(newPlan, flaw, newLink, asInsertLink.necessaryVariableConstraints)
+        }
+      } else if (resolver(i).isInstanceOf[EfficientInsertPlanStepWithLink]) {
         val asInsertTask = resolver(i).asInstanceOf[EfficientInsertPlanStepWithLink]
         // create new variables ... adding tasks to a plan also adds variables
         val newParameters: Array[Int] = new Array[Int](asInsertTask.newPlanStep._2.length)
@@ -56,12 +67,24 @@ case class EfficientOpenPrecondition(plan: EfficientPlan, planStep: Int, precond
 
         val newLink = asInsertTask.causalLink.addOffsetToPlanStepsIfGreaterThan(newTasks, plan.firstFreePlanStepID)
         newResolvers append EfficientInsertPlanStepWithLink(newPlan, flaw, newPlanStep, asInsertTask.parameterVariableSorts, newLink, newConstraints)
+      } else {
+        val asDecomposePlanStep = resolver(i).asInstanceOf[EfficientDecomposePlanStep]
+        //TODO recompute these
       }
       i += 1
     }
+
+    var possibleProducer = 2
+    while (possibleProducer < newPlan.firstFreePlanStepID) {
+      if (!newPlan.domain.tasks(newPlan.planStepTasks(possibleProducer)).isPrimitive && possibleProducer != planStep)
+        newResolvers appendAll EfficientDecomposePlanStep(newPlan, this, possibleProducer)
+      possibleProducer += 1
+    }
+
     // add new insert causal link modifications that become possible due to the new tasks
     newResolvers appendAll EfficientInsertCausalLink(newPlan, flaw, planStep, preconditionIndex, newPlan.planStepTasks.length - newTasks, newPlan.planStepTasks.length)
     flaw.precomputedResolver = Some(newResolvers.toArray)
     flaw
   }
+
 }
