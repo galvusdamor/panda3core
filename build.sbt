2b11ce8509bcaff79b004821ad04aeb909c9074c
Daniel <daniel.hoeller@uni-ulm.de>
2016-03-25 17:55:14 +0100

    changed command line interface of prefix component

--- build.sbt
+++ build.sbt
@@ -24,6 +24,7 @@ scalaVersion := "2.11.7"
 
 //mainClass in assembly := Some("de.uniulm.ki.panda3.efficient.search.BFS")
 mainClass in assembly := Some("de.uniulm.ki.panda3.translation.PANDAtranslator")
+//mainClass in assembly := Some("de.uniulm.ki.panda3.symbolic.compiler.prefix.PANDAaddPrefix")
 
 compileOrder in Compile := CompileOrder.Mixed
 
--- src/main/java/de/uniulm/ki/panda3/symbolic/compiler/prefix/PANDAaddPrefix.java
+++ src/main/java/de/uniulm/ki/panda3/symbolic/compiler/prefix/PANDAaddPrefix.java
@@ -9,13 +9,13 @@ import scala.Tuple2;
 
 public class PANDAaddPrefix {
 
-    private static final boolean debugMode = true;
+    private static final boolean debugMode = false;
 
     private static String domain_targetPath = null;
     private static String problem_targetPath = null;
 
-    private static String[][] prefix;
-    private static String[][] process;
+    private static String[][] prefix = null;
+    private static String[][] process = null;
 
     public static void main(String[] args) {
         System.out.println("[START]\tRepair-Transformation");
@@ -29,29 +29,15 @@ public class PANDAaddPrefix {
         if (debugMode) {
             String tempRunUUID = java.util.UUID.randomUUID().toString();
             System.out.println("[DEBUG]\tSet default call arguments.");
-
-            args = new String[]{"domain",
-                    "src/test/resources/de/uniulm/ki/panda3/symbolic/compiler/monroe.lisp",
-                    "/home/dhoeller/Schreibtisch/temp/monroe-d" + tempRunUUID + ".lisp",
-                    "problem",
-                    "src/test/resources/de/uniulm/ki/panda3/symbolic/compiler/p-0001-clear-road-wreck.lisp",
-                    "/home/dhoeller/Schreibtisch/temp/monroe-d" + tempRunUUID + ".lisp",
-/*                    "process",
-                    "-Connects",
-                    "Stuttart_London_Air_Route Stuttgart London",*/
-                    "prefix",
-                    //"navegate-vehicle climb-in",
-                    "navegate-vehicle navegate-vehicle climb-in",
-                    "wcrew1 wtruck1 brighton-dump texaco1",
-                    "wcrew1 wtruck1 brighton-dump texaco1",
-                    "tcrew1 wtruck1 brighton-dump",
-                    "end"};
+            args = getExampleCall();
         }
 
-        prefix = null;
-        process = null;
-
         try {
+            if (args.length == 0){
+                for (String line : getExampleCall())
+                System.out.print(line + " ");
+                return;
+            }
             Tuple2<Domain, Plan> domPlan = parseCallArguments(args);
             PrefixTransformer prefixTransformer;
             if (process == null)
@@ -64,9 +50,7 @@ public class PANDAaddPrefix {
             // transformation introduces nasty structures that should be removed
             domPlan = ToPlainFormulaRepresentation.transform(domPlan);
 
-            String toDomainFile = "/home/dhoeller/Schreibtisch/temp/dom.lisp";
-            String toProblemFile = "/home/dhoeller/Schreibtisch/temp/prob.lisp";
-            FileHandler.writeHPDDLToFiles(domPlan, toDomainFile, toProblemFile);
+            FileHandler.writeHPDDLToFiles(domPlan, domain_targetPath, problem_targetPath);
 
         } catch (addPrefixException error) {
             System.out.println("[ABORT]\tRepairing plan failed due to PlanRepairException: " + error.getMessage());
@@ -81,70 +65,72 @@ public class PANDAaddPrefix {
         System.out.println("[END]\tRepair-Transformation");
     }
 
+    private static String[] getExampleCall() {
+        return new String[]{"-domain",
+                "src/test/resources/de/uniulm/ki/panda3/symbolic/compiler/monroe.lisp",
+                "/home/dhoeller/Schreibtisch/temp/dom.lisp",
+                "-problem",
+                "src/test/resources/de/uniulm/ki/panda3/symbolic/compiler/p-0001-clear-road-wreck.lisp",
+                "/home/dhoeller/Schreibtisch/temp/prob.lisp",
+/*                    "-process",
+                "-Connects",
+                "Stuttart_London_Air_Route Stuttgart London",*/
+                "-prefix",
+                "(navegate-vehicle wcrew1 wtruck1 brighton-dump texaco1)" +
+                        "(navegate-vehicle wcrew1 wtruck1 brighton-dump texaco1)" +
+                        "(climb-in tcrew1 wtruck1 brighton-dump)",
+/*                    "navegate-vehicle navegate-vehicle climb-in",
+                "wcrew1 wtruck1 brighton-dump texaco1",
+                "wcrew1 wtruck1 brighton-dump texaco1",
+                "tcrew1 wtruck1 brighton-dump",*/
+                };
+    }
+
     private static Tuple2<Domain, Plan> parseCallArguments(String[] lines) throws Exception {
         String domain_sourcePath = "";
         String problem_sourcePath = "";
 
-        String[][] prefixPrimitiveTasks_Name_ArgumentValues = null;
-        String[][] processLiterals_Name_ArgumentValues = null;
-
-        boolean endLoop = false;
-
-        for (int a = 0; a < lines.length && !endLoop; a++) {
+        for (int a = 0; a < lines.length; a++) {
             switch (lines[a]) {
-                case "domain":
+                case "-domain":
                     a++;
                     domain_sourcePath = lines[a];
                     a++;
                     domain_targetPath = lines[a];
                     break;
-                case "problem":
+                case "-problem":
                     a++;
                     problem_sourcePath = lines[a];
                     a++;
                     problem_targetPath = lines[a];
                     break;
-                case "process":
+                case "-process":
                     a++;
                     String[] tempParts_1 = lines[a].split(" ");
-                    processLiterals_Name_ArgumentValues = new String[tempParts_1.length][];
+                    process = new String[tempParts_1.length][];
                     for (int b = 0; b < tempParts_1.length; b++) {
                         a++;
                         String[] tempSubParts_1 = lines[a].equals("") ? new String[0] : lines[a].split(" ");
-                        processLiterals_Name_ArgumentValues[b] = new String[tempSubParts_1.length + 1];
-                        processLiterals_Name_ArgumentValues[b][0] = tempParts_1[b];
+                        process[b] = new String[tempSubParts_1.length + 1];
+                        process[b][0] = tempParts_1[b];
                         for (int c = 0; c < tempSubParts_1.length; c++) {
-                            processLiterals_Name_ArgumentValues[b][c + 1] = tempSubParts_1[c];
+                            process[b][c + 1] = tempSubParts_1[c];
                         }
                     }
                     break;
-                case "prefix":
-                    a++;
-                    String[] tempParts_2 = lines[a].split(" ");
-                    prefixPrimitiveTasks_Name_ArgumentValues = new String[tempParts_2.length][];
-                    for (int b = 0; b < tempParts_2.length; b++) {
-                        a++;
-                        String[] tempSubParts_2 = lines[a].equals("") ? new String[0] : lines[a].split(" ");
-                        prefixPrimitiveTasks_Name_ArgumentValues[b] = new String[tempSubParts_2.length + 1];
-                        prefixPrimitiveTasks_Name_ArgumentValues[b][0] = tempParts_2[b];
-                        for (int c = 0; c < tempSubParts_2.length; c++) {
-                            prefixPrimitiveTasks_Name_ArgumentValues[b][c + 1] = tempSubParts_2[c];
-                        }
+                case "-prefix":
+                    String prefixStr = lines[++a];
+                    // want to match: "(climb-in a b)(navegate-vehicle b t g) (someother f t),(test z p), (other a w)"
+                    String[] prefixSplit = prefixStr.substring(1, prefixStr.length() - 1).split("(\\)\\(|\\) \\(|\\)\\,\\(|\\)\\, \\()");
+                    prefix = new String[prefixSplit.length][];
+                    for (int i = 0; i < prefixSplit.length; i++) {
+                        String[] action = prefixSplit[i].split(" ");
+                        prefix[i] = action;
                     }
-                    break;
-                case "end":
-                    endLoop = true;
                     break;
                 default:
                     throw new addPrefixException("Invalid command '" + lines[a] + "'.");
             }
-        }
-
-        prefix = prefixPrimitiveTasks_Name_ArgumentValues;
-        process = processLiterals_Name_ArgumentValues;
-
-        if (!endLoop) {
-            System.out.println("[Warning]\t'end' expected.");
         }
 
         return FileHandler.loadHDDLFromFile(domain_sourcePath, problem_sourcePath);
