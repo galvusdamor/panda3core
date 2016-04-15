bf53e075fafcb4a097747879171d3ef80c6b751e
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-04-15 19:22:38 +0200

    fixed the plugins file

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,3 @@
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.5")
+
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
---
+++ src/main/scala/de/uniulm/ki/panda3/efficient/domain/datastructures/EfficientTaskSchemaTransitionGraph.scala
@@ -0,0 +1,8 @@
+package de.uniulm.ki.panda3.efficient.domain.datastructures
+
+/**
+  * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
+  */
+class EfficientTaskSchemaTransitionGraph {
+
+}
