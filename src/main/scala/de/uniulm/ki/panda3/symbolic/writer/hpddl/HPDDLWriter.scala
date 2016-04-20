829ef69f13d13ae95d9a6d06f0a0d48a68dd65a0
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-04-20 20:11:18 +0200

    Extracted the toPDDLvariable name function

--- src/main/scala/de/uniulm/ki/panda3/symbolic/writer/hpddl/HPDDLWriter.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/writer/hpddl/HPDDLWriter.scala
@@ -6,6 +6,8 @@ import de.uniulm.ki.panda3.symbolic.logic._
 import de.uniulm.ki.panda3.symbolic.plan.Plan
 import de.uniulm.ki.panda3.symbolic.plan.element.{OrderingConstraint, PlanStep}
 import de.uniulm.ki.panda3.symbolic.writer.Writer
+import de.uniulm.ki.panda3.symbolic.writer._
+
 
 /**
  * This is a writer for the hierarchical PDDL format created by Ronald W. Alford (ronwalf@volus.net)
@@ -13,11 +15,6 @@ import de.uniulm.ki.panda3.symbolic.writer.Writer
  * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
  */
 case class HPDDLWriter(domainName: String, problemName: String) extends Writer {
-
-  def toPDDLIdentifier(id: String): String = {
-    val removedSigns = id map { c => if (c == '?') c else if (c >= 'a' && c <= 'z') c else if (c >= 'A' && c <= 'Z') c else if (c >= '0' && c <= '9') c else '_'}
-    if (removedSigns.charAt(0) >= '0' && removedSigns.charAt(0) <= '9') "p" + removedSigns else removedSigns
-  }
 
   def toHPDDLVariableName(name: String): String = toPDDLIdentifier(if (name.startsWith("?")) name else "?" + name)
 
