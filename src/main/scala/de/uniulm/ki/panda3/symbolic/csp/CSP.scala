3cccfa555656d6ac6db1260c2e1a2d8f30ada0b9
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-04-29 20:28:30 +0200

    CSP bugfix

--- src/main/scala/de/uniulm/ki/panda3/symbolic/csp/CSP.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/csp/CSP.scala
@@ -237,7 +237,7 @@ case class CSP(variables: Set[Variable], constraints: Seq[VariableConstraint]) e
   /** executes uni propagation on a set of given variables */
   private def unitPropagation(toPropagate: Iterable[Variable] = detectUnitPropagation()): Unit = if (toPropagate.isEmpty) {() }
   else {
-    val x: Iterable[Variable] = (toPropagate flatMap { variable =>
+    val x: Set[Variable] = (toPropagate flatMap { variable =>
       if (!isPotentiallySolvable) {Vector() }
       else {
         val constant = remainingDomain(variable).last // this one exists and is the only element
@@ -266,7 +266,8 @@ case class CSP(variables: Set[Variable], constraints: Seq[VariableConstraint]) e
       }
     }).toSet // eliminate duplicates
 
-    if (isPotentiallySolvable) unitPropagation(x)
+    // subtraction seems to be necessary as another variable can issue a propagare of a not yet propagated variable
+    if (isPotentiallySolvable) unitPropagation(x -- toPropagate)
     checkIntegrity()
   }
 
