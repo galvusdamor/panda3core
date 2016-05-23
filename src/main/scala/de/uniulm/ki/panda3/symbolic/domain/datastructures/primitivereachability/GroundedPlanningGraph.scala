587f9743fcd22cafa38d42d2b3db9ce4b33619d1
Kristof Mickeleit <Kristof Mickeleit>
2016-05-23 08:56:21 +0200

    substituted if-then-else with pattern matching, minor improvements

--- src/main/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/primitivereachability/GroundedPlanningGraph.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/primitivereachability/GroundedPlanningGraph.scala
@@ -33,10 +33,7 @@ class GroundedPlanningGraph(domain: Domain, initialState: Set[GroundLiteral], co
 
       val assignMap: Map[Variable, Constant] = Map()
       //  Instantiate actions which become available because of the new propositions "union" deletion of mutexes
-      val newActions: Set[GroundTask] = (newPropositions flatMap { (gl: GroundLiteral) => {
-        domain.consumersOf.getOrElse(gl.predicate, Seq.empty[ReducedTask]) flatMap { (t: ReducedTask) => createActionInstances(t, assignMap, gl, (t.precondition.conjuncts find { (l: Literal) => l.predicate == gl.predicate }).get, t.precondition.conjuncts) }
-      }
-      }) union (deletedMutexes flatMap { (t: (GroundLiteral, GroundLiteral)) => Set(t._1, t._2) } flatMap { (gl: GroundLiteral) => {
+      val newActions: Set[GroundTask] = ((newPropositions ++ (deletedMutexes flatMap { (t: (GroundLiteral, GroundLiteral)) => Set(t._1, t._2) })) flatMap { (gl: GroundLiteral) => {
         domain.consumersOf.getOrElse(gl.predicate, Seq.empty[ReducedTask]) flatMap { (t: ReducedTask) => createActionInstances(t, assignMap, gl, (t.precondition.conjuncts find { (l: Literal) => l.predicate == gl.predicate }).get, t.precondition.conjuncts) }
       }
       })
@@ -45,24 +42,26 @@ class GroundedPlanningGraph(domain: Domain, initialState: Set[GroundLiteral], co
      * TODO: Check the correctness for special cases.
      */
       def createActionInstances(task: ReducedTask, assignMap: Map[Variable, Constant], gl: GroundLiteral, l: Literal, precons: Seq[Literal]): Set[GroundTask] = {
-        if (gl == Nil) {
-          Set.empty[GroundTask]
-        } else {
-          // Check the variable-constant-Mapping
-          val correct: Boolean = ((l.parameterVariables zip gl.parameter) map { t: (Variable, Constant) => (assignMap.get(t._1) == t._2) }).foldLeft(false)((b1: Boolean, b2: Boolean) => b1 || b2)
-          if (correct) {
-            val updatedAssignMap = assignMap ++ (l.parameterVariables zip gl.parameter).toMap
-            val updatedPrecons = (precons filterNot { (lit: Literal) => lit == l })
-            if (updatedPrecons.size == 0) {
-              /*
-             * TODO: Convert the updatedAssignMap back to a Seq[Constant] in the correct order.
-             */
-              Set(GroundTask(task, ???))
-            } else {
-              (updatedPrecons flatMap { (lit: Literal) => preconMap.getOrElse(lit.predicate, Set.empty[GroundLiteral]) flatMap { (newgl: GroundLiteral) => createActionInstances(task, updatedAssignMap, newgl, lit, updatedPrecons) } }).toSet
+        gl match {
+          case null => Set.empty[GroundTask]
+          case _ => {
+            val correct: Boolean = ((l.parameterVariables zip gl.parameter) map { t: (Variable, Constant) => (assignMap.get(t._1) == t._2) }).foldLeft(false)((b1: Boolean, b2: Boolean) => b1 || b2)
+            correct match {
+              case true => {
+                val updatedAssignMap = assignMap ++ (l.parameterVariables zip gl.parameter).toMap
+                val updatedPrecons = (precons filterNot { (lit: Literal) => lit == l })
+                updatedPrecons.size match {
+                  case 0 => {
+                    /*
+                    * TODO: Convert the updatedAssignMap back to a Seq[Constant] in the correct order.
+                    */
+                    Set(GroundTask(task, ???))
+                  }
+                  case _ => (updatedPrecons flatMap { (lit: Literal) => preconMap.getOrElse(lit.predicate, Set.empty[GroundLiteral]) flatMap { (newgl: GroundLiteral) => createActionInstances(task, updatedAssignMap, newgl, lit, updatedPrecons) } }).toSet
+                }
+              }
+              case false => Set.empty[GroundTask]
             }
-          } else {
-            Set.empty[GroundTask]
           }
         }
       }
