cfde8b4295a7303a29f967130bdcdfcab69bcd30
Kristof Mickeleit <Kristof Mickeleit>
2016-05-25 13:04:43 +0200

    bugfixes, improvements, new test

--- src/main/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/primitivereachability/GroundedPlanningGraph.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/primitivereachability/GroundedPlanningGraph.scala
@@ -7,7 +7,6 @@ import de.uniulm.ki.panda3.symbolic.logic._
 import de.uniulm.ki.panda3.symbolic.plan.element.GroundTask
 
 import collection.mutable.{HashMap, MultiMap}
-import scala.collection.mutable
 
 /**
   * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
@@ -23,16 +22,22 @@ case class GroundedPlanningGraph(domain: Domain, initialState: Set[GroundLiteral
   lazy val layerWithMutexes: Seq[(Set[GroundTask], Set[(GroundTask, GroundTask)], Set[GroundLiteral], Set[(GroundLiteral, GroundLiteral)])] = {
 
     def buildGraph(previousLayer: (Set[GroundTask], Set[(GroundTask, GroundTask)], Set[GroundLiteral], Set[(GroundLiteral, GroundLiteral)]),
-                   addedPropositions: Set[GroundLiteral], deletedMutexes: Set[(GroundLiteral, GroundLiteral)]):
+                   addedPropositions: Set[GroundLiteral], deletedMutexes: Set[(GroundLiteral, GroundLiteral)],firstLayer: Boolean):
                    Seq[(Set[GroundTask], Set[(GroundTask, GroundTask)], Set[GroundLiteral], Set[(GroundLiteral, GroundLiteral)])] = {
-
       fillPreconMap(addedPropositions)
       //  Instantiate actions which become available because of new propositions and deletion of mutexes
-      val newActions: Set[GroundTask] = addedPropositions ++ (deletedMutexes flatMap { mutex => Set(mutex._1, mutex._2) }) flatMap { groundLiteral => {
+      val newActions1: Set[GroundTask] = addedPropositions ++ (deletedMutexes flatMap { mutex => Set(mutex._1, mutex._2) }) flatMap { groundLiteral => {
         domain.consumersOf(groundLiteral.predicate) flatMap { task =>
-        createActionInstances(task, groundLiteral, task.precondition.conjuncts find { literal => literal.predicate == groundLiteral.predicate } get,
+        createActionInstances(task, groundLiteral, (task.precondition.conjuncts find { literal => literal.predicate == groundLiteral.predicate }).get,
         task.precondition.conjuncts, previousLayer._4) } }
       }
+
+      val newActions2: Set[GroundTask] = firstLayer match {
+        case true  => createActionInstancesForTasksWithoutPreconditions(domain.tasks collect {
+          case t: ReducedTask => t } filter { reducedTask => reducedTask.precondition.conjuncts.isEmpty})
+        case false => Set.empty[GroundTask]
+      }
+      val newActions: Set[GroundTask] = newActions1 ++ newActions2
       val newNoOps: Set[GroundTask] = addedPropositions map { groundLiteral => createNOOP(groundLiteral)}
 
       val allActions: Set[GroundTask] = previousLayer._1 ++ newActions ++ newNoOps
@@ -57,13 +62,27 @@ case class GroundedPlanningGraph(domain: Domain, initialState: Set[GroundLiteral
       if (newPropositions.isEmpty && previousLayer._4.size == allPropositionMutexes.size) {
         Seq(thisLayer)
       } else {
-        thisLayer +: buildGraph(thisLayer, newPropositions, previousLayer._4 diff allPropositionMutexes)
+        thisLayer +: buildGraph(thisLayer, newPropositions, previousLayer._4 diff allPropositionMutexes, false)
       }
+    }
+
+    def createActionInstancesForTasksWithoutPreconditions(tasks: Seq[ReducedTask]): Set[GroundTask] = {
+      (tasks flatMap { task => computeAllPossibleSubstitutionCombinations(task.parameters) map { combination => GroundTask(task, combination)}}).toSet
+    }
+
+    def computeAllPossibleSubstitutionCombinations(variables: Seq[Variable]): Seq[Seq[Constant]] = {
+      val variablesWithPossibleSubstitutions: Seq[(Variable, Seq[Constant])] = variables map { variable => (variable, variable.sort.elements )}
+      def computeSubstitutionCombinations[A](sets:Seq[Seq[A]]) : Seq[Seq[A]] = sets match {
+        case Nil => List(Nil)
+        case s+:ss => computeSubstitutionCombinations(ss).flatMap(s2 => s.map(_ +: s2)) }
+
+      computeSubstitutionCombinations(variablesWithPossibleSubstitutions map { case (v,c) => c})
     }
 
     def createActionInstances(task: ReducedTask, groundLiteral: GroundLiteral, literal: Literal, unassignedPrecons: Seq[Literal],
                               mutexes: Set[(GroundLiteral, GroundLiteral)], assignMap: Map[Variable, Constant] = Map(), groundLiterals: Seq[GroundLiteral] = Seq.empty[GroundLiteral]):
                               Set[GroundTask] = {
+
       val updatedPrecons: Seq[Literal] = unassignedPrecons filterNot { _ == literal }
       val updatedGroundLiterals = groundLiterals :+ groundLiteral
       val assignPairs: Seq[(Variable, Constant)] = literal.parameterVariables zip groundLiteral.parameter
@@ -71,18 +90,15 @@ case class GroundedPlanningGraph(domain: Domain, initialState: Set[GroundLiteral
 
       //Check if all preconditions of the task have been assigned
       if(updatedPrecons.isEmpty){
+
         //Check if all parameters of the task have been assigned
         if(task.parameters.size == updatedAssignMap.keys.size) {
           val arguments: Seq[Constant] = task.parameters map { variable => updatedAssignMap(variable)}
           Set(GroundTask(task, arguments))
         } else {
           val unassignedVariables: Seq[Variable] = task.parameters filterNot{ variable => updatedAssignMap.keySet contains variable}
-          val variablesWithPossibleSubstitutions: Seq[(Variable, Seq[Constant])] = unassignedVariables map { variable => (variable, variable.sort.elements )}
 
-          def computeSubstitutionCombinations[A](sets:Seq[Seq[A]]) : Seq[Seq[A]] = sets match { case Nil => List(Nil)
-          case s+:ss => computeSubstitutionCombinations(ss).flatMap(s2 => s.map(_ +: s2)) }
-
-          val possibleSubstitutionCombinations: Seq[Seq[Constant]] = computeSubstitutionCombinations(variablesWithPossibleSubstitutions map { case (v,c) => c})
+          val possibleSubstitutionCombinations: Seq[Seq[Constant]] = computeAllPossibleSubstitutionCombinations(unassignedVariables)
 
           val possibleSubstitutionCombinationsWithVariables: Seq[Seq[(Variable, Constant)]] = possibleSubstitutionCombinations map { seq => unassignedVariables.zip(seq)}
           val allArgumentCombinations: Seq[Seq[Constant]] = possibleSubstitutionCombinationsWithVariables map { combination =>
@@ -107,7 +123,7 @@ case class GroundedPlanningGraph(domain: Domain, initialState: Set[GroundLiteral
 
     def fillPreconMap(propositions: Set[GroundLiteral]): Unit = propositions.foreach(p => preconMap.addBinding(p.predicate, p))
 
-    buildGraph((Set.empty[GroundTask], Set.empty[(GroundTask, GroundTask)], initialState, Set.empty[(GroundLiteral, GroundLiteral)]), initialState, Set.empty[(GroundLiteral, GroundLiteral)])
+    buildGraph((Set.empty[GroundTask], Set.empty[(GroundTask, GroundTask)], initialState, Set.empty[(GroundLiteral, GroundLiteral)]), initialState, Set.empty[(GroundLiteral, GroundLiteral)], true)
   }
   private val preconMap = new HashMap[Predicate, collection.mutable.Set[GroundLiteral]] with MultiMap[Predicate, GroundLiteral]
 
--- src/test/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/primitivereachability/GroundedPlanningGraphTest.scala
+++ src/test/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/primitivereachability/GroundedPlanningGraphTest.scala
@@ -23,12 +23,16 @@ class GroundedPlanningGraphTest extends FlatSpec {
     val (domain, initialPlan) = ToPlainFormulaRepresentation.transform(cwaAppliedDomainAndProblem, ())
 
 
-    val groundedInitialState = initialPlan.groundedInitialState filter { _.isPositive }
+    val groundedInitialState = initialPlan.groundedInitialState filter {
+      _.isPositive
+    }
     val planningGraph = new GroundedPlanningGraph(domain, groundedInitialState.toSet, true, false, Left(Nil))
 
 
     assert(planningGraph.graphSize == 3)
-    assert(planningGraph.reachableGroundLiterals exists { _.predicate.name == "d" })
+    assert(planningGraph.reachableGroundLiterals exists {
+      _.predicate.name == "d"
+    })
   }
 
   it must "recognise impossible situations" in {
@@ -44,12 +48,38 @@ class GroundedPlanningGraphTest extends FlatSpec {
     val (domain, initialPlan) = RemoveNegativePreconditions.transform(plainFormula, ())
 
 
-    val groundedInitialState = initialPlan.groundedInitialState filter { _.isPositive }
+    val groundedInitialState = initialPlan.groundedInitialState filter {
+      _.isPositive
+    }
     val planningGraph = new GroundedPlanningGraph(domain, groundedInitialState.toSet, true, false, Left(Nil))
 
 
     assert(planningGraph.graphSize == 3) // TODO: check whether this is correct manually!
-    assert(!(planningGraph.reachableGroundLiterals exists { _.predicate.name == "d" }))
+    assert(!(planningGraph.reachableGroundLiterals exists {
+      _.predicate.name == "d"
+    }))
+  }
+
+  it must "instantiate actions with parameters not contained in preconditions" in {
+
+    val domainFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/domain/primitivereachability/planningGraphTest03_domain.hddl"
+    val problemFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/domain/primitivereachability/planningGraphTest03_problem.hddl"
+
+    val parsedDomainAndProblem = HDDLParser.parseDomainAndProblem(new FileInputStream(domainFile), new FileInputStream(problemFile))
+    // we assume that the domain is grounded
+
+    // cwa
+    val cwaAppliedDomainAndProblem = ClosedWorldAssumption.transform(parsedDomainAndProblem, ())
+    val plainFormula = ToPlainFormulaRepresentation.transform(cwaAppliedDomainAndProblem, ())
+    val (domain, initialPlan) = RemoveNegativePreconditions.transform(plainFormula, ())
+
+
+    val groundedInitialState = initialPlan.groundedInitialState filter {
+      _.isPositive
+    }
+    val planningGraph = new GroundedPlanningGraph(domain, groundedInitialState.toSet, true, false, Left(Nil))
+
+    assert(planningGraph.reachableGroundPrimitiveActions exists { _.task.name == "v"})
   }
 
 }
