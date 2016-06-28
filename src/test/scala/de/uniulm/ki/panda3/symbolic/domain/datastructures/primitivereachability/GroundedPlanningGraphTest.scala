15d8501ef94870629f8cc13d8ba01781ed24b9fc
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-06-28 14:06:48 +0200

    added another TC for the planning graph, which breaks it

--- src/test/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/primitivereachability/GroundedPlanningGraphTest.scala
+++ src/test/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/primitivereachability/GroundedPlanningGraphTest.scala
@@ -71,9 +71,9 @@ class GroundedPlanningGraphTest extends FlatSpec {
     assert(!(planningGraph.reachableGroundLiterals exists {
       _.predicate.name == "d"
     }))
-    assert(planningGraph.layerWithMutexes.last._4 exists { case (gl1, gl2) => (gl1.predicate.name == "+a") && ( gl2.predicate.name == "+c")})
-    assert(planningGraph.layerWithMutexes.last._4 exists { case (gl1, gl2) => (gl1.predicate.name == "+c") && ( gl2.predicate.name == "+e")})
-    assert(planningGraph.layerWithMutexes.last._4 exists { case (gl1, gl2) => (gl1.predicate.name == "+c") && ( gl2.predicate.name == "+b")})
+    assert(planningGraph.layerWithMutexes.last._4 exists { case (gl1, gl2) => (gl1.predicate.name == "+a") && (gl2.predicate.name == "+c") })
+    assert(planningGraph.layerWithMutexes.last._4 exists { case (gl1, gl2) => (gl1.predicate.name == "+c") && (gl2.predicate.name == "+e") })
+    assert(planningGraph.layerWithMutexes.last._4 exists { case (gl1, gl2) => (gl1.predicate.name == "+c") && (gl2.predicate.name == "+b") })
 
   }
 
@@ -95,7 +95,7 @@ class GroundedPlanningGraphTest extends FlatSpec {
     }
     val planningGraph = new GroundedPlanningGraph(domain, groundedInitialState.toSet, true, false, Left(Nil))
 
-    assert(planningGraph.reachableGroundPrimitiveActions exists { _.task.name == "v"})
+    assert(planningGraph.reachableGroundPrimitiveActions exists { _.task.name == "v" })
   }
 
   it must "handle sorts without objects" in {
@@ -111,9 +111,7 @@ class GroundedPlanningGraphTest extends FlatSpec {
     val (domain, initialPlan) = RemoveNegativePreconditions.transform(plainFormula, ())
 
 
-    val groundedInitialState = initialPlan.groundedInitialState filter {
-      _.isPositive
-    }
+    val groundedInitialState = initialPlan.groundedInitialState filter { _.isPositive }
     val planningGraph = new GroundedPlanningGraph(domain, groundedInitialState.toSet, true, false, Left(Nil))
 
     assert(planningGraph.graphSize == 1)
@@ -132,9 +130,7 @@ class GroundedPlanningGraphTest extends FlatSpec {
     val (domain, initialPlan) = RemoveNegativePreconditions.transform(plainFormula, ())
 
 
-    val groundedInitialState = initialPlan.groundedInitialState filter {
-      _.isPositive
-    }
+    val groundedInitialState = initialPlan.groundedInitialState filter { _.isPositive }
     val planningGraph = new GroundedPlanningGraph(domain, groundedInitialState.toSet, true, false, Left(Nil))
 
     assert(planningGraph.graphSize == 1)
@@ -153,21 +149,19 @@ class GroundedPlanningGraphTest extends FlatSpec {
     val (domain, initialPlan) = RemoveNegativePreconditions.transform(plainFormula, ())
 
 
-    val groundedInitialState = initialPlan.groundedInitialState filter {
-      _.isPositive
-    }
-    val forbiddenLiftedTasks: Seq[ReducedTask] = (domain.tasks filter { case t:ReducedTask => t.name == "Y"}).asInstanceOf[Seq[ReducedTask]]
+    val groundedInitialState = initialPlan.groundedInitialState filter { _.isPositive }
+    val forbiddenLiftedTasks: Seq[ReducedTask] = (domain.tasks filter { case t: ReducedTask => t.name == "Y" }).asInstanceOf[Seq[ReducedTask]]
     val forbiddenTasks = Right(forbiddenLiftedTasks)
     val planningGraph = new GroundedPlanningGraph(domain, groundedInitialState.toSet, true, false, forbiddenTasks)
 
     assert(planningGraph.graphSize == 1)
-    assert(!(planningGraph.layerWithMutexes.last._1 exists { groundTask => groundTask.task.name == "Y"}))
+    assert(!(planningGraph.layerWithMutexes.last._1 exists { groundTask => groundTask.task.name == "Y" }))
 
-    val forbiddenGroundTasks: Seq[GroundTask] = Seq(GroundTask(forbiddenLiftedTasks.head,Seq.empty[Constant]))
+    val forbiddenGroundTasks: Seq[GroundTask] = Seq(GroundTask(forbiddenLiftedTasks.head, Seq.empty[Constant]))
     val forbiddenTasks2 = Left(forbiddenGroundTasks)
     val planningGraph2 = new GroundedPlanningGraph(domain, groundedInitialState.toSet, true, false, forbiddenTasks2)
 
-    assert(!(planningGraph2.layerWithMutexes.last._1 contains forbiddenGroundTasks.head ))
+    assert(!(planningGraph2.layerWithMutexes.last._1 contains forbiddenGroundTasks.head))
   }
   it must "not compute mutexes if required" in {
     val domainFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/domain/primitivereachability/planningGraphTest01_domain.hddl"
@@ -181,25 +175,33 @@ class GroundedPlanningGraphTest extends FlatSpec {
     val (domain, initialPlan) = ToPlainFormulaRepresentation.transform(cwaAppliedDomainAndProblem, ())
 
 
-    val groundedInitialState = initialPlan.groundedInitialState filter {
-      _.isPositive
-    }
+    val groundedInitialState = initialPlan.groundedInitialState filter { _.isPositive }
     val planningGraph = new GroundedPlanningGraph(domain, groundedInitialState.toSet, false, false, Left(Nil))
 
-    assert(planningGraph.reachableGroundLiterals exists {
-      _.predicate.name == "a"
-    })
-    assert(planningGraph.reachableGroundLiterals exists {
-      _.predicate.name == "b"
-    })
-    assert(planningGraph.reachableGroundLiterals exists {
-      _.predicate.name == "c"
-    })
-    assert(planningGraph.reachableGroundLiterals exists {
-      _.predicate.name == "d"
-    })
+    assert(planningGraph.reachableGroundLiterals exists { _.predicate.name == "a" })
+    assert(planningGraph.reachableGroundLiterals exists { _.predicate.name == "b" })
+    assert(planningGraph.reachableGroundLiterals exists { _.predicate.name == "c" })
+    assert(planningGraph.reachableGroundLiterals exists { _.predicate.name == "d" })
     assert(planningGraph.layerWithMutexes.last._2.isEmpty)
     assert(planningGraph.layerWithMutexes.last._4.isEmpty)
   }
 
+  it must "handle constants in actions correctly" in {
+    val domainFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/domain/primitivereachability/planningGraphTest06_domain.hddl"
+    val problemFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/domain/primitivereachability/planningGraphTest06_problem.hddl"
+
+    val parsedDomainAndProblem = HDDLParser.parseDomainAndProblem(new FileInputStream(domainFile), new FileInputStream(problemFile))
+    // we assume that the domain is grounded
+
+    // cwa
+    val cwaAppliedDomainAndProblem = ClosedWorldAssumption.transform(parsedDomainAndProblem, ())
+    val (domain, initialPlan) = ToPlainFormulaRepresentation.transform(cwaAppliedDomainAndProblem, ())
+
+
+    val groundedInitialState = initialPlan.groundedInitialState filter { _.isPositive }
+    val planningGraph = new GroundedPlanningGraph(domain, groundedInitialState.toSet, true, false, Left(Nil))
+
+    assert(planningGraph.graphSize == 1)
+    assert(planningGraph.reachableGroundPrimitiveActions.isEmpty)
+  }
 }
