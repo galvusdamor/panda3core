2e634824006d36d8f4cf317b4a3540855316780c
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-07-08 10:39:49 +0200

    dot compiler now uses temporary files

--- src/main/scala/de/uniulm/ki/util/Dot2PdfCompiler.scala
+++ src/main/scala/de/uniulm/ki/util/Dot2PdfCompiler.scala
@@ -1,5 +1,7 @@
 package de.uniulm.ki.util
 
+import java.io.File
+
 import sys.process._
 
 /**
@@ -11,10 +13,19 @@ object Dot2PdfCompiler {
 
   def writeDotToFile[X](dotObject: DotPrintable[X], file: String): Unit = writeDotToFile(dotObject.dotString, file)
 
-  def writeDotToFile(dotString: String, file: String): Unit = {
-   // println(dotString)
-    val prepString = dotString.replaceAll("\n", "\\\\n").replaceAll("\t", "\\\\t").replaceAll("'", "\\'")
-    ("echo -e " + prepString + "") #| ("dot -Tpdf -o " + file) !!
+  def writeDotToFile(dotString: String, file: String): Unit = System.getProperty("os.name") match {
+    case "NEVER" => //"Linux"
+      // println(dotString)
+      val prepString = dotString.replaceAll("\n", "\\\\n").replaceAll("\t", "\\\\t").replaceAll("'", "\\'")
+      ("echo -e " + prepString + "") #| ("dot -Tpdf -o " + file) !!
+
+    case _ =>
+      val tempFile = File.createTempFile("__panda_dot_print", "dot")
+
+      de.uniulm.ki.util.writeStringToFile(dotString, tempFile)
+
+      ("dot -Tpdf " + tempFile.getAbsolutePath + " -o " + file) !!
+
   }
 }
 
@@ -22,6 +33,6 @@ trait DotPrintable[OptionType] {
   /** The DOT representation of the object */
   val dotString: String
 
-  /** The DOT representation of the object with options*/
-  def dotString(options : OptionType) : String
+  /** The DOT representation of the object with options */
+  def dotString(options: OptionType): String
 }
