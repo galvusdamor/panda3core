6dea74eef2acfe5996ade97ba064009f8fca8f47
Daniel <daniel.hoeller@uni-ulm.de>
2016-07-11 19:25:53 +0200

    switched on sort preprocessing

--- src/main/java/de/uniulm/ki/panda3/progression/PANDApro.java
+++ src/main/java/de/uniulm/ki/panda3/progression/PANDApro.java
@@ -2,6 +2,7 @@ package de.uniulm.ki.panda3.progression;
 
 import de.uniulm.ki.panda3.progression.proSearch.planningInstance;
 import de.uniulm.ki.panda3.symbolic.compiler.ClosedWorldAssumption;
+import de.uniulm.ki.panda3.symbolic.compiler.ExpandSortHierarchy;
 import de.uniulm.ki.panda3.symbolic.compiler.RemoveNegativePreconditions;
 import de.uniulm.ki.panda3.symbolic.compiler.ToPlainFormulaRepresentation;
 import de.uniulm.ki.panda3.symbolic.domain.Domain;
@@ -25,12 +26,20 @@ public class PANDApro {
         //String problemFile = "/home/dhoeller/Dokumente/repositories/private/papers/2017-panda-pro/domains/simple-finite-problem-2.lisp";
         //String domainFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/domain/primitivereachability/planningGraphTest01_domain.hddl";
         //String problemFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/domain/primitivereachability/planningGraphTest01_problem.hddl";
-        String domainFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/parser/pddl/IPC6/pegsol-strips/domain/p05-domain.pddl";
-        String problemFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/parser/pddl/IPC6/pegsol-strips/problems/p05.pddl";
+        //String domainFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/parser/pddl/IPC6/pegsol-strips/domain/p25-domain.pddl";
+        //String problemFile = "src/test/resources/de/uniulm/ki/panda3/symbolic/parser/pddl/IPC6/pegsol-strips/problems/p25.pddl";
 
+        // STRIPS
+        // String domainFile = "/home/dhoeller/Dokumente/repositories/private/papers/2017-panda-pro/domains/lifted-htn/domain-strips.lisp";
+        // String problemFile = "/home/dhoeller/Dokumente/repositories/private/papers/2017-panda-pro/domains/lifted-htn/p01-strips.lisp";
+
+        // HTN
+        String domainFile = "../panda3core_with_planning_graph/src/test/resources/de/uniulm/ki/panda3/symbolic/parser/hpddl/htn-strips-pairs/domain-htn.lisp";
+        String problemFile = "../panda3core_with_planning_graph/src/test/resources/de/uniulm/ki/panda3/symbolic/parser/hpddl/htn-strips-pairs/p01-htn.lisp";
 
         Tuple2<Domain, Plan> instance = FileHandler.loadHDDLFromFile(domainFile, problemFile);
 
+        instance = ExpandSortHierarchy.transform(instance._1(), instance._2());
         instance = ClosedWorldAssumption.transform(instance._1(), instance._2());
         instance = ToPlainFormulaRepresentation.transform(instance._1(), instance._2());
         instance = RemoveNegativePreconditions.transform(instance._1(), instance._2());
