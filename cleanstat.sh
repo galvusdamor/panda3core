1b419900200ea8671ac398d2a289362926dbea69
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-08-26 16:21:34 +0200

    added a script which produces a more clean git statistics, i.e. one not including generated code, domain and problem files, and other test files not containing code

---
+++ cleanstat.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+git pull
+
+cat << 'EOF' > /tmp/cleancommit
+#!/bin/bash
+
+find . -name "*.pddl" -type f -delete
+find . -name "*.xml" -type f -delete
+find . -name "*.hddp" -type f -delete
+find . -name "*.in" -type f -delete
+find . -name "*.ans" -type f -delete
+find . -name "*.lisp" -type f -delete
+find . -name "*.tex" -type f -delete
+find . -name "*.hpddl" -type f -delete
+find . -name "*antlr*Parser.java" -type f -delete
+find . -name "*antlr*Lexer.java" -type f -delete
+find . -name "*hddlParser.java" -type f -delete
+find . -name "*hddlLexer.java" -type f -delete
+find . -name "*.tokens" -type f -delete
+find . -path "*/java/*/xml/*" -type f -delete
+
+EOF
+
+chmod +x /tmp/cleancommit
+
+# remove all the files from previously created list in current repository
+git filter-branch -f --tree-filter '/tmp/cleancommit' HEAD
+
+rm /tmp/cleancommit
+
+# now execute your gitstats command (should be fixed for your needs)
+gitstats .git stats
+
+# reset all the changes filter-branch have done
+git reset --hard refs/original/refs/heads/master
