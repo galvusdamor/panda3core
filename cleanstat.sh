7aff17f77ba652a3f8f9d615f320b0ab09141bce
Galvus Damor <gregor.behnke@uni-ulm.de>
2016-09-20 20:04:29 +0200

    better git statistics script

--- cleanstat.sh
+++ cleanstat.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
-git fetch
-git rebase
+git clone https://mirkwood.informatik.uni-ulm.de/gitbucket/git/gbehnke/panda3core.git
+wget http://repo1.maven.org/maven2/com/madgag/bfg/1.12.13/bfg-1.12.13.jar
 
-cat << 'EOF' > /tmp/cleancommit
-#!/bin/bash
 
-find . -name "*.pddl" -type f -delete
-find . -name "*.PDDL" -type f -delete
-find . -name "*.xml" -type f -delete
-find . -name "*.hddp" -type f -delete
-find . -name "*.in" -type f -delete
-find . -name "*.ans" -type f -delete
-find . -name "*.lisp" -type f -delete
-find . -name "*.tex" -type f -delete
-find . -name "*.hpddl" -type f -delete
-find . -name "*antlr*Parser.java" -type f -delete
-find . -name "*antlr*Lexer.java" -type f -delete
-find . -name "*hddlParser.java" -type f -delete
-find . -name "*hddlLexer.java" -type f -delete
-find . -name "*.tokens" -type f -delete
-find . -path "*/java/*/xml/*" -type f -delete
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.pddl" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.PDDL" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.xml" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.hddp" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.in" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.ans" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.lisp" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.tex" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.hpddl" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*antlr*Parser.java" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*antlr*Lexer.java" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*hddlParser.java" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*hddlLexer.java" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "*.tokens" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "pfile*" panda3core
+java -jar bfg-1.12.13.jar --no-blob-protection --delete-files "Domain.java" panda3core
 
-EOF
 
-chmod +x /tmp/cleancommit
+find . -path "*/java/*/xml/*java" -type f -exec sh -c 'echo $(basename {})' \; | sort  | uniq | while read f
+do
+	java -jar bfg-1.12.13.jar --no-blob-protection --delete-files $f panda3core
+done
 
-# remove all the files from previously created list in current repository
-git filter-branch -f --tree-filter '/tmp/cleancommit' HEAD
-
-rm /tmp/cleancommit
+cd panda3core
+git reflog expire --expire=now --all && git gc --prune=now --aggressive
+cd ..
 
 # now execute your gitstats command (should be fixed for your needs)
-#gitstats .git stats
+gitstats panda3core stats
+
+rm -rf panda3core bfg-1.12.13.jar
 
 # reset all the changes filter-branch have done
 #git reset --hard origin/master
