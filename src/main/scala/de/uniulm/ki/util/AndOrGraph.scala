db3594de256611299101432b877984f644e0b7d1
Galvus Damor <gregor.behnke@uni-ulm.de>
2017-02-03 19:46:45 +0100

    added a version of the AndOrGraph knowing that it operates on ints and can thus do minimax more efficiently

--- src/main/scala/de/uniulm/ki/util/AndOrGraph.scala
+++ src/main/scala/de/uniulm/ki/util/AndOrGraph.scala
@@ -89,7 +89,6 @@ trait AndOrGraph[T, A <: T, O <: T] extends DirectedGraphWithAlgorithms[T] {
     seen collect { case (a: A, v) if andVertices contains a => a -> v } toMap
   }
 
-
   override protected def dotVertexStyleRenderer(v: T): String = v match {
     case a: A if andEdges.contains(a) => ",shape = box, style = filled, fillcolor = red"
     case o: O if orEdges.contains(o)  => ""
@@ -106,4 +105,111 @@ case class SimpleAndOrGraph[T, A <: T, O <: T](andVertices: Set[A], orVertices: 
   orEdges foreach { _._2 foreach { a => assert(andVertices contains a) } }
   assert(andVertices.size == andEdges.size, andVertices.size + "!=" + andEdges.size)
   assert(orVertices.size == orEdges.size, orVertices.size + "!=" + orEdges.size)
+}
+
+case class IntegerAntOrGraph(andVertices: Set[Int], orVertices: Set[Int], andEdges: Map[Int, Set[Int]], orEdges: Map[Int, Set[Int]]) extends AndOrGraph[Int, Int, Int] {
+  andEdges foreach { a => assert(andVertices contains a._1) }
+  orEdges foreach { o => assert(orVertices contains o._1) }
+  andEdges foreach { _._2 foreach { o => assert(orVertices contains o) } }
+  orEdges foreach { _._2 foreach { a => assert(andVertices contains a) } }
+  assert(andVertices.size == andEdges.size, andVertices.size + "!=" + andEdges.size)
+  assert(orVertices.size == orEdges.size, orVertices.size + "!=" + orEdges.size)
+
+  assert(andVertices forall { _ >= 0 })
+
+  private lazy val maximumValue   : Int = (andVertices ++ orVertices).max
+  private lazy val minimumValue   : Int = (andVertices ++ orVertices).min
+  private lazy val maximumAndValue: Int = andVertices.max
+
+  private lazy val offset           = -1 * minimumValue
+  private lazy val indexArrayLength = maximumValue + offset + 1
+
+  private lazy val reversedCondensedTopologicalOrderingArray: Array[Array[Int]] = condensation.topologicalOrdering.get.reverse map { _.toArray } toArray
+
+  private lazy val isAndVertex: Array[Boolean] = Range(0, indexArrayLength) map { i => andVertices.contains(i - offset) } toArray
+  private lazy val isOrVertex : Array[Boolean] = Range(0, indexArrayLength) map { i => orVertices.contains(i - offset) } toArray
+
+  private lazy val adjacencyList: Array[Array[Int]] = Range(0, indexArrayLength) map { i =>
+    val actualIndex = i - offset
+    edges(actualIndex).toArray
+  } toArray
+
+  def minSumTraversalArray(evaluate: (Int => Double), sumInitialValue: (Int => Double)): Array[Double] = {
+    val seen: Array[Double] = new Array[Double](indexArrayLength)
+    var i = 0
+    while (i < indexArrayLength) {
+      seen(i) = -1
+      i += 1
+    }
+
+    def mini(root: Int): Boolean =
+      if (adjacencyList(root + offset).length == 0) {
+        val v = evaluate(root)
+        seen(root + offset) = v
+        false
+      } else {
+        var value: Double = Integer.MAX_VALUE
+        var pos = 0
+        while (pos < adjacencyList(root + offset).length) {
+          value = Math.min(value, seen(adjacencyList(root + offset)(pos) + offset))
+          pos += 1
+        }
+
+        if (seen(root + offset) != value) {
+          seen(root + offset) = value // side effect
+          true
+        } else false
+      }
+
+    def sum(root: Int): Boolean = {
+      var value = sumInitialValue(root)
+      var pos = 0
+      while (pos < adjacencyList(root + offset).length) {
+        value += seen(adjacencyList(root + offset)(pos) + offset)
+        pos += 1
+      }
+
+      if (seen(root + offset) != value) {
+        seen(root + offset) = value
+        true
+      } else false
+    }
+
+
+    var sccIndex = 0
+    while (sccIndex < reversedCondensedTopologicalOrderingArray.length) {
+      val scc = reversedCondensedTopologicalOrderingArray(sccIndex)
+
+      var sccElement = 0
+      while (sccElement < scc.length) {
+        seen(scc(sccElement) + offset) = Integer.MAX_VALUE
+        sccElement += 1
+      }
+
+      var changed = true
+      while (changed) {
+        // reset
+        changed = false
+
+        sccElement = 0
+        while (sccElement < scc.length) {
+          val task = scc(sccElement)
+          if (isAndVertex(task + offset)) changed |= mini(task)
+          else if(isOrVertex(task + offset)) changed |= sum (task)
+          sccElement += 1
+        }
+      }
+      sccIndex += 1
+    }
+
+    // copy seen
+    val result = new Array[Double](maximumAndValue + 1)
+    i = 0
+    while (i <= maximumAndValue) {
+      result(i) = seen(i + offset)
+      i += 1
+    }
+
+    result
+  }
 }
