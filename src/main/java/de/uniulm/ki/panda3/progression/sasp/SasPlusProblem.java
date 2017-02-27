b2602598cf695f65c488ccfe551b9a38e65ec839
Daniel <daniel.hoeller@uni-ulm.de>
2017-02-27 19:03:09 +0100

    sas+ support

--- src/main/java/de/uniulm/ki/panda3/progression/sasp/SasPlusProblem.java
+++ src/main/java/de/uniulm/ki/panda3/progression/sasp/SasPlusProblem.java
@@ -4,46 +4,93 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 /**
- * Created by dh on 24.02.17.
+ * Created by Daniel Höller on 24.02.17.
  */
 public class SasPlusProblem {
+    /**
+     * Class to parse Malte Helmert's SAS+ format
+     * <p>
+     * see: http://www.fast-downward.org/TranslatorOutputFormat
+     */
     private static String noVal = "<none of those>";
 
     private int version;
     private int error = 0;
     private boolean actionCosts = false;
 
-    // number of elemets
-    private int numberOfVars;
-    private int numberOfMutexGroups;
-    private int numberOfGoalPairs;
-    private int numberOfOperators;
+    // number of elements
+    private int numOfVars;
+    private int numOfMutexGroups;
+    private int numOfGoalPairs;
+    private int numOfOperators;
 
     public int[] axioms;
-    public int[] ranges;
-    private String[] varNames;
-    private String[][] values;
+    public int[] ranges; // variable-index -> number of values the var can have
+    private String[] varNames; // variable-index ->  name-string of the var
+    private String[][] values; // var-index, value-index -> value-string (this is either an atom from the domain, or the noVal-String)
 
-    private int[] s0;
-    private int[][] goal;
+    private int[] s0; // variable-index -> value set in s0
+    private int[][] goal; // enum of pairs [var-index, value-needed]
 
-    private String[] opNames;
-    private int[] numPrevailConditions;
-    private int[][][] prevailConditions; // operator, conditionId ->
-    int[] numEffects;
-    int[] costs;
-    private int[][] numberOfEffectConditions;
-    private int[][][] effectConditions; // operatorId, effectId -> pair(int valueId, int value)
-    private int[][] effectVar; // operatorId, effectId -> int variableId
-    private int[][] effectVarPrec; // operatorId, effectId -> int variableId
-    private int[][] effectVarEff;  // operatorId, effectId -> int variableId
+    private String[] opNames; // operator-index -> name-string
+
+    /* prevail conditions vs. preconditions vs. effect condition
+     *
+     * - prevail conditions must hold for the operator to be applicable (aka STRIPS-precondition), but do not show up
+     *   in the effect
+     * - preconditions describe the value a variable that DO show up in the effect must have (or -1 if there is no
+     *   such value) (this is also a STRIPS-precondition)
+     * - the effect condition is an precondition of a conditional effect (that might be included in the domain or be
+     *   produced by some transformation)
+     */
+
+    // prevail conditions
+    private int[] numPrevailConditions; // operator-index -> number of prevail-conditions
+    private int[][][] prevailConditions; // operator-index, condition-index -> pair [variable-index, value]
+
+    // effect conditions (aka STRIPS-conditional-effects)
+    private int[][] numberOfEffectConditions; // operator-index -> number of effect-conditions
+    private int[][][] effectConditions; // operator-index, effect-index -> pair [variable-index, value]
+
+    /* effects
+     * - some variable needs to have value before-hand and gets a new value afterwards
+     * - i.e. in a STRIPS manner, the following lines include preconditions as well as effects
+     */
+    private int[] numEffects; // operator-index -> number of effects
+    private int[][] effectVar; // operator-index, effect-index -> variable-id
+    private int[][] effectVarPrec; // operator-index, effect-index -> old var index (a precondition)
+    private int[][] effectVarEff;  // operator-index, effect-index -> new var index (the effect)
+
+    private int[] costs; // operator-index -> action costs
+
+    // our representation
+    private int numOfStateFeatures; // num of features used in our representation
+    private int[] firstIndex; // maps a mutex-group-index to the first index in the vector of state features
+    private int[] lastIndex; // maps a mutex-group-index to the last index in the vector of state features
+
+    // translations
+    // int -> Object
+    // Object -> int
+    // action in old model <-> action in new model
+
+    public void prepareInteralRep() {
+        numOfStateFeatures = 0;
+        firstIndex = new int[numOfVars];
+        lastIndex = new int[numOfVars];
+
+        for (int ivar = 0; ivar < numOfVars; ivar++) {
+            firstIndex[ivar] = numOfStateFeatures;
+            numOfStateFeatures += ranges[ivar];
+            lastIndex[ivar] = numOfStateFeatures - 1;
+        }
+    }
 
     public SasPlusProblem(String Filename) throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(Filename));
         String line = br.readLine();
         if (!line.equals("begin_version")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Did not find version element.");
+            System.out.println("Error: (SAS+ parser) Did not find version element.");
             return;
         }
         readVersion(br);
@@ -51,26 +98,26 @@ public class SasPlusProblem {
 
         if (!line.equals("begin_metric")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Did not find metric element.");
+            System.out.println("Error: (SAS+ parser) Did not find metric element.");
             return;
         }
         readMetric(br);
 
         // read variable definition
-        numberOfVars = Integer.parseInt(br.readLine());
-        varNames = new String[numberOfVars];
-        axioms = new int[numberOfVars];
-        ranges = new int[numberOfVars];
-        values = new String[numberOfVars][];
+        numOfVars = Integer.parseInt(br.readLine());
+        varNames = new String[numOfVars];
+        axioms = new int[numOfVars];
+        ranges = new int[numOfVars];
+        values = new String[numOfVars][];
 
-        for (int i = 0; i < numberOfVars; i++) {
+        for (int i = 0; i < numOfVars; i++) {
             readVariable(br, i);
         }
 
         // read mutex groups
-        numberOfMutexGroups = Integer.parseInt(br.readLine());
+        numOfMutexGroups = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < numberOfMutexGroups; i++) {
+        for (int i = 0; i < numOfMutexGroups; i++) {
             readMutex(br, i);
         }
         line = br.readLine();
@@ -78,7 +125,7 @@ public class SasPlusProblem {
         // read s0 and goal
         if (!line.equals("begin_state")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Did not find (initial) state element.");
+            System.out.println("Error: (SAS+ parser) Did not find (initial) state element.");
             return;
         }
         readS0(br);
@@ -86,26 +133,26 @@ public class SasPlusProblem {
 
         if (!line.equals("begin_goal")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Did not find goal element.");
+            System.out.println("Error: (SAS+ parser) Did not find goal element.");
             return;
         }
         readGoal(br);
         line = br.readLine();
 
         // read operators
-        this.numberOfOperators = Integer.parseInt(br.readLine());
-        this.opNames = new String[this.numberOfOperators];
-        this.numEffects = new int[this.numberOfOperators];
-        this.numPrevailConditions = new int[this.numberOfOperators];
-        this.prevailConditions = new int[this.numberOfOperators][][];
-        this.numberOfEffectConditions = new int[this.numberOfOperators][];
-        this.effectConditions = new int[this.numberOfOperators][][];
-        this.effectVar = new int[this.numberOfOperators][];
-        this.effectVarPrec = new int[this.numberOfOperators][];
-        this.effectVarEff = new int[this.numberOfOperators][];
-        this.costs = new int[this.numberOfOperators];
+        this.numOfOperators = Integer.parseInt(br.readLine());
+        this.opNames = new String[this.numOfOperators];
+        this.numEffects = new int[this.numOfOperators];
+        this.numPrevailConditions = new int[this.numOfOperators];
+        this.prevailConditions = new int[this.numOfOperators][][];
+        this.numberOfEffectConditions = new int[this.numOfOperators][];
+        this.effectConditions = new int[this.numOfOperators][][];
+        this.effectVar = new int[this.numOfOperators][];
+        this.effectVarPrec = new int[this.numOfOperators][];
+        this.effectVarEff = new int[this.numOfOperators][];
+        this.costs = new int[this.numOfOperators];
 
-        for (int i = 0; i < this.numberOfOperators; i++) {
+        for (int i = 0; i < this.numOfOperators; i++) {
             readOperator(br, i);
         }
     }
@@ -113,7 +160,7 @@ public class SasPlusProblem {
     private void readOperator(BufferedReader br, int i) throws Exception {
         if (!br.readLine().equals("begin_operator")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Unexpected structure of operator element.");
+            System.out.println("Error: (SAS+ parser) Unexpected structure of operator element.");
         }
 
         this.opNames[i] = br.readLine();
@@ -130,7 +177,7 @@ public class SasPlusProblem {
         this.numEffects[i] = Integer.parseInt(br.readLine());
         numberOfEffectConditions[i] = new int[this.numEffects[i]];
         effectVar[i] = new int[this.numEffects[i]];
-        effectVarPrec[i] = new int[this.numEffects[i]];
+        effectVarPrec[i] = new int[this.numEffects[i]]; // this may be -1, which means that there is no specific value needed
         effectVarEff[i] = new int[this.numEffects[i]];
 
         for (int j = 0; j < this.numEffects[i]; j++) {
@@ -151,14 +198,14 @@ public class SasPlusProblem {
 
         if (!br.readLine().equals("end_operator")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Unexpected structure of operator element.");
+            System.out.println("Error: (SAS+ parser) Unexpected structure of operator element.");
         }
     }
 
     private void readGoal(BufferedReader br) throws Exception {
-        this.numberOfGoalPairs = Integer.parseInt(br.readLine());
-        this.goal = new int[this.numberOfGoalPairs][];
-        for (int i = 0; i < this.numberOfGoalPairs; i++) {
+        this.numOfGoalPairs = Integer.parseInt(br.readLine());
+        this.goal = new int[this.numOfGoalPairs][];
+        for (int i = 0; i < this.numOfGoalPairs; i++) {
             String[] pair = br.readLine().trim().split(" ");
             this.goal[i] = new int[2];
             this.goal[i][0] = Integer.parseInt(pair[0]);
@@ -167,13 +214,13 @@ public class SasPlusProblem {
     }
 
     private void readS0(BufferedReader br) throws Exception {
-        this.s0 = new int[this.numberOfVars];
-        for (int i = 0; i < this.numberOfVars; i++) {
+        this.s0 = new int[this.numOfVars];
+        for (int i = 0; i < this.numOfVars; i++) {
             this.s0[i] = Integer.parseInt(br.readLine());
         }
         if (!br.readLine().equals("end_state")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Unexpected structure of (initial) state element.");
+            System.out.println("Error: (SAS+ parser) Unexpected structure of (initial) state element.");
         }
     }
 
@@ -185,7 +232,7 @@ public class SasPlusProblem {
     private void readVariable(BufferedReader br, int i) throws Exception {
         if (!br.readLine().equals("begin_variable")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Unexpected structure of variable element.");
+            System.out.println("Error: (SAS+ parser) Unexpected structure of variable element.");
         }
         varNames[i] = br.readLine();
         axioms[i] = Integer.parseInt(br.readLine());
@@ -199,14 +246,14 @@ public class SasPlusProblem {
             } else {
                 if (!val.startsWith("Atom ")) {
                     this.error = 1;
-                    System.out.println("Error: (SAS+-parser) Unexpected structure of variable element.");
+                    System.out.println("Error: (SAS+ parser) Unexpected structure of variable element.");
                 }
                 values[i][j] = val.substring("Atom ".length(), val.length());
             }
         }
         if (!br.readLine().equals("end_variable")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Unexpected structure of variable element.");
+            System.out.println("Error: (SAS+ parser) Unexpected structure of variable element.");
         }
     }
 
@@ -217,7 +264,7 @@ public class SasPlusProblem {
             this.actionCosts = true;
         if ((m < 0) || (m > 1) || (!br.readLine().equals("end_metric"))) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Unexpected structure of metric element.");
+            System.out.println("Error: (SAS+ parser) Unexpected structure of metric element.");
         }
 
     }
@@ -227,7 +274,7 @@ public class SasPlusProblem {
         this.version = Integer.parseInt(v);
         if (!br.readLine().equals("end_version")) {
             this.error = 1;
-            System.out.println("Error: (SAS+-parser) Unexpected structure of version element.");
+            System.out.println("Error: (SAS+ parser) Unexpected structure of version element.");
         }
     }
 
@@ -242,7 +289,7 @@ public class SasPlusProblem {
             sb.append("no ");
         sb.append("action costs\n\n");
 
-        for (int i = 0; i < numberOfVars; i++) {
+        for (int i = 0; i < numOfVars; i++) {
             sb.append(varNames[i]);
             sb.append(" {");
             for (int j = 0; j < ranges[i]; j++) {
@@ -256,7 +303,7 @@ public class SasPlusProblem {
         sb.append("Skipped mutex groups\n\n");
 
         sb.append("s0 {");
-        for (int i = 0; i < numberOfVars; i++) {
+        for (int i = 0; i < numOfVars; i++) {
             if (i > 0)
                 sb.append("; ");
             sb.append(varNames[i]);
@@ -266,7 +313,7 @@ public class SasPlusProblem {
         sb.append("}\n\n");
 
         sb.append("g {");
-        for (int i = 0; i < numberOfGoalPairs; i++) {
+        for (int i = 0; i < numOfGoalPairs; i++) {
             if (i > 0)
                 sb.append("; ");
             sb.append(varNames[goal[i][0]]);
@@ -275,7 +322,7 @@ public class SasPlusProblem {
         }
         sb.append("}\n\n");
 
-        for (int i = 0; i < numberOfOperators; i++) {
+        for (int i = 0; i < numOfOperators; i++) {
             sb.append(opNames[i]);
             sb.append(" {\n");
             sb.append("Prevail-Cond: {");
--- src/main/java/de/uniulm/ki/panda3/progression/sasp/testSASP.java
+++ src/main/java/de/uniulm/ki/panda3/progression/sasp/testSASP.java
@@ -5,7 +5,7 @@ package de.uniulm.ki.panda3.progression.sasp;
  */
 public class testSASP {
     public static void main(String[] str) throws Exception {
-        String s = "/home/dh/Schreibtisch/sasp/output.sas";
+        String s = "/home/dh/Schreibtisch/sasp/org/output.sas";
         SasPlusProblem sasp = new SasPlusProblem(s);
         System.out.println(sasp.toString());
     }
