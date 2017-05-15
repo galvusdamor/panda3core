1345dea0acc33087c6dc9223d6c7197aea690b32
Daniel <daniel.hoeller@uni-ulm.de>
2017-05-15 12:38:42 +0200

    efficient solution representation

---
+++ src/main/java/de/uniulm/ki/panda3/progression/htn/search/searchRoutine/SolutionStep.java
@@ -0,0 +1,77 @@
+package de.uniulm.ki.panda3.progression.htn.search.searchRoutine;
+
+import de.uniulm.ki.panda3.progression.htn.search.ProgressionNetwork;
+import de.uniulm.ki.panda3.symbolic.domain.SimpleDecompositionMethod;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by dh on 15.05.17.
+ */
+public class SolutionStep {
+
+    private SolutionStep predecessor = null;
+
+    // my step
+    private int action;
+    private SimpleDecompositionMethod method;
+
+    private int length;
+    private int primitiveCount;
+    private int shopCount;
+
+    public SolutionStep() {
+        length = 0;
+        primitiveCount = 0;
+        shopCount = 0;
+    }
+
+    public SolutionStep(SolutionStep predSol, int action) {
+        this.predecessor = predSol;
+        this.action = action;
+        this.length = predSol.getLength() + 1;
+
+        if (!ProgressionNetwork.ShopPrecActions.contains(action))
+            primitiveCount++;
+        else
+            shopCount++;
+    }
+
+    public SolutionStep(SolutionStep predSol, SimpleDecompositionMethod method) {
+        this.predecessor = predSol;
+        this.method = method;
+        this.length = predSol.getLength() + 1;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public boolean isFirst() {
+        return (predecessor == null);
+    }
+
+    public int getLength() {
+        return this.length;
+    }
+
+    public int getPrimitiveCount() {
+        return primitiveCount;
+    }
+
+    public List<Object> getSolution() {
+        if (this.isFirst())
+            return new LinkedList<>();
+        else {
+            List<Object> l = predecessor.getSolution();
+            Object o;
+            if (this.method != null)
+                o = this.method;
+            else o = this.action;
+            ((LinkedList) l).addFirst(o);
+            return l;
+        }
+    }
+}
