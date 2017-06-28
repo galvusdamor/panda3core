5fe9d3a19e2152b20c356ccd9a257f7f9b975e9e
Galvus Damor <gregor.behnke@uni-ulm.de>
2017-06-28 18:17:18 +0200

    Removed LTL2BA dependency - I'll write the translation myself

--- src/main/scala/de/uniulm/ki/panda3/symbolic/sat/ltl/BüchiAutomaton.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/sat/ltl/BüchiAutomaton.scala
@@ -1,10 +1,6 @@
 package de.uniulm.ki.panda3.symbolic.sat.ltl
 
 import de.uniulm.ki.panda3.symbolic.domain.Task
-import rwth.i2.ltl2ba4j.LTL2BA4J
-import rwth.i2.ltl2ba4j.model.ITransition
-
-import scala.collection.JavaConversions
 
 /**
   * @author Gregor Behnke (gregor.behnke@uni-ulm.de)
@@ -15,14 +11,6 @@ case class BüchiAutomaton(numberOfStates: Int, initialState : Int, finalStates 
 
 object BüchiAutomaton {
   def apply(formula: String): BüchiAutomaton = {
-    // use LTL2BA4j
-    //val formulaFactory = new rwth.i2.ltl2ba4j.formula.impl.FormulaFactory()
-    //val f = ff.G(ff.Proposition("test"))
-
-    // TODO as of now, we are just outputting shit
-    val transitions: Seq[ITransition] = JavaConversions.collectionAsScalaIterable(LTL2BA4J.formulaToBA(formula)).toSeq
-
-    transitions foreach { t => println(t) }
 
 
 
