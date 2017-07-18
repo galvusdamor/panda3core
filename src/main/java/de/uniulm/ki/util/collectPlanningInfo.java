dc73dbf2b4d443003eb0092456d76a7e181228cb
Daniel <daniel.hoeller@uni-ulm.de>
2017-07-18 19:05:23 +0200

    cleanup

--- src/main/java/de/uniulm/ki/util/collectPlanningInfo.java
+++ src/main/java/de/uniulm/ki/util/collectPlanningInfo.java
@@ -130,9 +130,6 @@ public class collectPlanningInfo {
     }
 
     private static boolean isNumber(char c) {
-        //return (c == '0') || (c == '1') || (c == '2') || (c == '3') || (c == '4') ||
-        //        (c == '5') || (c == '6') || (c == '7') || (c == '8') || (c == '9');
-
         return c >= '0' && c <= '9';
     }
 }
