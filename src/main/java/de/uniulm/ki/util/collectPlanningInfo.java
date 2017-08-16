0f2708fe94a494c227a7d64c96aa4e915985c0d1
Daniel <daniel.hoeller@uni-ulm.de>
2017-08-16 11:26:56 +0200

    added info text that can be given as command line argument

--- src/main/java/de/uniulm/ki/util/collectPlanningInfo.java
+++ src/main/java/de/uniulm/ki/util/collectPlanningInfo.java
@@ -22,14 +22,25 @@ public class collectPlanningInfo {
         //String outFile = "/media/dh/Volume/repositories/private-documents/evaluation-domains/monroe/monroe-100-corpus/greedy-imp-random/greedy-imp-random.csv";
         //String inputFile = "/media/dh/Volume/repositories/private-documents/evaluation-domains/monroe/monroe-100-corpus/07-partial-observable/04-results-rcg-greedy/all.log";
         //String outFile = "/media/dh/Volume/repositories/private-documents/evaluation-domains/monroe/monroe-100-corpus/07-partial-observable/04-results-rcg-greedy/all.out";
+        //String inputFile = "/home/dh/Schreibtisch/test-csv/AStar_ADD_R_LCFR.log";
+        //String outFile = "/home/dh/Schreibtisch/test-csv/AStar_ADD_R_LCFR.csv";
         String inputFile = args[0]; //"foo";
         String outFile = args[1]; // "foo.csv";
         //String inputFile = "/home/gregor/Workspace/Panda3/lifted_planner/transport.log"; //"foo";
         //String outFile = "test.csv"; // "foo.csv";
+        //String inputFile = "/home/dh/Dokumente/data-AAAI2018/00-logs/plannerRun27-astar-pro-add.log";
+        //String outFile = "/home/dh/Dokumente/data-AAAI2018/plannerRun27-astar-pro-add.csv";
+
+        String infoText = "";
+        if (args.length > 2)
+            infoText = args[2];
+
         FileReader fr = new FileReader(inputFile);
         BufferedReader br = new BufferedReader(fr);
 
         HashMap<String, Boolean> isBoolsch = new HashMap<>();
+        if (infoText.length() > 0)
+            isBoolsch.put("infoTxt", false);
 
         List<Map<String, String>> allExps = new LinkedList<>();
         HashMap<String, String> oneExp = new HashMap<>();
@@ -46,13 +57,15 @@ public class collectPlanningInfo {
                 int lastNum = num;
                 num = Integer.parseInt(numStr);
 
-                if (num < lastNum) {
+                if (num <= lastNum) {
+                    if (infoText.length() > 0)
+                        oneExp.put("infoTxt", infoText);
                     allExps.add(oneExp);
                     oneExp = new HashMap<>();
                 }
 
                 csv = csv.trim();
-                String[] elems = csv.substring(1, csv.length() -1).split("\";\"");
+                String[] elems = csv.substring(1, csv.length() - 1).split("\";\"");
                 for (String elem : elems) {
                     String[] pair = elem.split("\"=\"");
                     if (pair.length == 2) {
@@ -74,6 +87,9 @@ public class collectPlanningInfo {
                 oneExp.put("postProcTLT", temp[temp.length - 1]);
             }
         }
+        if (infoText.length() > 0)
+            oneExp.put("infoTxt", infoText);
+
         allExps.add(oneExp);
 
         br.close();
