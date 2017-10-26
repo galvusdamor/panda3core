c0ecc2e8b1ec458d79d606f877e73f97ea5b6a17
Galvus Damor <gregor.behnke@uni-ulm.de>
2017-10-26 16:58:15 +0200

    BUGFIX ... carthesian product of 2-way-tdg never really worked ..

--- src/main/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/hierarchicalreachability/TwoStepDecompositionGraph.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/domain/datastructures/hierarchicalreachability/TwoStepDecompositionGraph.scala
@@ -99,7 +99,9 @@ case class TwoStepDecompositionGraph(domain: Domain, initialPlan: Plan, grounded
             val newVariableBindingList = nextPlanStep.arguments zip groundTask.arguments
             val newVariableBinding = newVariableBindingList.toMap
 
-            if (nextPlanStep.arguments exists { nv => if (newVariableBinding.contains(nv) && variableBinding.contains(nv)) newVariableBinding(nv) != variableBinding(nv) else false })
+            if (newVariableBinding exists { case (v, c) => !parameter(v).contains(c) })
+              Nil
+            else if (nextPlanStep.arguments exists { nv => if (newVariableBinding.contains(nv) && variableBinding.contains(nv)) newVariableBinding(nv) != variableBinding(nv) else false })
               Nil
             else {
               val newBinding = variableBinding ++ newVariableBinding
@@ -157,10 +159,10 @@ case class TwoStepDecompositionGraph(domain: Domain, initialPlan: Plan, grounded
             // fill map
             simpleMethod.subPlan.variableConstraints.variables foreach {
               v =>
-                val cspPossibleValues = simpleMethod.subPlan.variableConstraints.reducedDomainOf(v)
-                if (currentGroundTask.argumentMap contains v) cspPossibleValues intersect currentGroundTask.argumentMap(v) else cspPossibleValues
+                val cspPossibleValues : Seq[Constant] = simpleMethod.subPlan.variableConstraints.reducedDomainOf(v)
+                val reducedPossibleValues : Seq[Constant] = if (currentGroundTask.argumentMap contains v) cspPossibleValues intersect currentGroundTask.argumentMap(v) else cspPossibleValues
 
-                setParameter(v) = cspPossibleValues.toSet
+                setParameter(v) = reducedPossibleValues.toSet
             }
             // propagate equality
             var changed = true
@@ -184,11 +186,12 @@ case class TwoStepDecompositionGraph(domain: Domain, initialPlan: Plan, grounded
       val flattenedPossibleMethods = possibleMethods map { _._2 }
       cartMethodsMap(currentGroundTask) = cartMethodsMap(currentGroundTask) ++ flattenedPossibleMethods
       flattenedPossibleMethods foreach {
-        cartMethod => cartMethod.subTasks foreach {
-          subTask =>
-            cartTaskInMethodsMap(subTask) = cartTaskInMethodsMap(subTask) + cartMethod
+        cartMethod =>
+          cartMethod.subTasks foreach {
+            subTask =>
+              cartTaskInMethodsMap(subTask) = cartTaskInMethodsMap(subTask) + cartMethod
             //println(cartTaskInMethodsMap(subTask).size)
-        }
+          }
       }
 
 
@@ -208,72 +211,75 @@ case class TwoStepDecompositionGraph(domain: Domain, initialPlan: Plan, grounded
     // go through the reachable ground tasks and add to data structure
     val possibleGroundInstances = new mutable.HashMap[CartesianGroundTask, Set[GroundTask]]().withDefaultValue(Set())
     groundedReachabilityAnalysis.reachableGroundPrimitiveActions foreach {
-      gt => cartTasksMap(gt.task) foreach {
-        ct =>
-          // only add if the ground action is a valid instantiation of the cartesian action
-          if (ct.parameter zip gt.arguments forall {
-            case (cs, c) => cs contains c
-          }) possibleGroundInstances(ct) = possibleGroundInstances(ct) + gt
-      }
+      gt =>
+        cartTasksMap(gt.task) foreach {
+          ct =>
+            // only add if the ground action is a valid instantiation of the cartesian action
+            if (ct.parameter zip gt.arguments forall {
+              case (cs, c) => cs contains c
+            }) possibleGroundInstances(ct) = possibleGroundInstances(ct) + gt
+        }
     }
 
     // run the actual grounding procedure
     taskOrdering foreach {
-      scc => if (scc.size != 1 || scc.head.isAbstract) {
-        //println("\n\n\n\n\nSCC " + (scc map { _.name }))
+      scc =>
+        if (scc.size != 1 || scc.head.isAbstract) {
+          //println("\n\n\n\n\nSCC " + (scc map { _.name }))
 
-        def groundNew(cartesianGroundMethod: CartesianGroundMethod): Seq[(Task, GroundTask)] = {
-          //println("GROUND A NEW " + cartesianGroundMethod.method.name + " " + (cartesianGroundMethod.subTasks map { gt => gt.shortInfo + " " + possibleGroundInstances(gt).size })
-          //  .mkString("  "))
+          def groundNew(cartesianGroundMethod: CartesianGroundMethod): Seq[(Task, GroundTask)] = {
+            //println("GROUND A NEW " + cartesianGroundMethod.method.name + " " + (cartesianGroundMethod.subTasks map { gt => gt.shortInfo + " " + possibleGroundInstances(gt).size })
+            //  .mkString("  "))
+            //println("RESTRICT " + (cartesianGroundMethod.parameter map {case (v,c) => v.name + " -> " + c.size}).mkString(" "))
 
-          var nc = 0
+            var nc = 0
 
-          val newMethods = cartesianGroundMethod.groundWithPossibleTasks(possibleGroundInstances)
-          //println("DONE " + newMethods.size)
-          var actuallyNew = 0
-          val r = newMethods flatMap {
-            newMethod =>
-              cartTasksMap(cartesianGroundMethod.abstractTask.task) foreach {
-                case possCT =>
-                  if (possCT isCompatible newMethod.groundAbstractTask) if (!(possibleGroundInstances(possCT) contains newMethod.groundAbstractTask)) {
-                    possibleGroundInstances(possCT) = possibleGroundInstances(possCT) + newMethod.groundAbstractTask
-                    //println("ADD TO " + possCT.shortInfo )
-                    nc += 1
-                  }
-              }
+            val newMethods = cartesianGroundMethod.groundWithPossibleTasks(possibleGroundInstances)
+            //println("DONE " + newMethods.size)
+            var actuallyNew = 0
+            val r = newMethods flatMap {
+              newMethod =>
+                cartTasksMap(cartesianGroundMethod.abstractTask.task) foreach {
+                  case possCT =>
+                    if (possCT isCompatible newMethod.groundAbstractTask) if (!(possibleGroundInstances(possCT) contains newMethod.groundAbstractTask)) {
+                      possibleGroundInstances(possCT) = possibleGroundInstances(possCT) + newMethod.groundAbstractTask
+                      //println("ADD TO " + possCT.shortInfo )
+                      nc += 1
+                    }
+                }
 
-              if (!(methodsMap contains newMethod.groundAbstractTask)) {
-                methodsMap(newMethod.groundAbstractTask) = Set(newMethod)
-                //println("START " + (newMethod.variableBinding map { case (v,c) => v.name + "->" + c.name} mkString " "))
-                actuallyNew += 1
-                (newMethod.groundAbstractTask.task, newMethod.groundAbstractTask) :: Nil
-              } else if (!(methodsMap(newMethod.groundAbstractTask) contains newMethod)) {
-                methodsMap(newMethod.groundAbstractTask) = methodsMap(newMethod.groundAbstractTask) + newMethod
-                actuallyNew += 1
-                //println("ADD "  + (newMethod.variableBinding map { case (v,c) => v.name + "->" + c.name} mkString " "))
-                Nil
-              } else Nil
+                if (!(methodsMap contains newMethod.groundAbstractTask)) {
+                  methodsMap(newMethod.groundAbstractTask) = Set(newMethod)
+                  //println("START " + (newMethod.variableBinding map { case (v,c) => v.name + "->" + c.name} mkString " "))
+                  actuallyNew += 1
+                  (newMethod.groundAbstractTask.task, newMethod.groundAbstractTask) :: Nil
+                } else if (!(methodsMap(newMethod.groundAbstractTask) contains newMethod)) {
+                  methodsMap(newMethod.groundAbstractTask) = methodsMap(newMethod.groundAbstractTask) + newMethod
+                  actuallyNew += 1
+                  //println("ADD "  + (newMethod.variableBinding map { case (v,c) => v.name + "->" + c.name} mkString " "))
+                  Nil
+                } else Nil
+            }
+
+            //println("Actually " + nc + " N " + actuallyNew)
+            r
           }
 
-          //println("Actually " + nc + " N " + actuallyNew)
-          r
+          var untreatedTaskGroundings: Set[(Task, GroundTask)] =
+            scc flatMap { task => cartTasksMap(task) flatMap { cartTask => cartMethodsMap(cartTask) } } flatMap { cartMethod => groundNew(cartMethod) }
+
+          while (untreatedTaskGroundings.nonEmpty) {
+            //println("ITERATE")
+            val triggers: Set[(Task, Seq[GroundTask])] =
+              untreatedTaskGroundings groupBy { _._1 } map { case (task, taskAndGroundTasks) => (task, taskAndGroundTasks map { _._2 } toSeq) } toSet
+
+            untreatedTaskGroundings = triggers flatMap {
+              case (task, groundTasks) =>
+                //println("ONE TRIGGER " + groundTasks.length)
+                cartTasksMap(task) flatMap { cartTask => cartTaskInMethodsMap(cartTask) } flatMap groundNew
+            } filter { case (t, _) => scc contains t }
+          }
         }
-
-        var untreatedTaskGroundings: Set[(Task, GroundTask)] =
-          scc flatMap { task => cartTasksMap(task) flatMap { cartTask => cartMethodsMap(cartTask) } } flatMap { cartMethod => groundNew(cartMethod) }
-
-        while (untreatedTaskGroundings.nonEmpty) {
-          //println("ITERATE")
-          val triggers: Set[(Task, Seq[GroundTask])] =
-            untreatedTaskGroundings groupBy { _._1 } map { case (task, taskAndGroundTasks) => (task, taskAndGroundTasks map { _._2 } toSeq) } toSet
-
-          untreatedTaskGroundings = triggers flatMap {
-            case (task, groundTasks) =>
-              //println("ONE TRIGGER " + groundTasks.length)
-              cartTasksMap(task) flatMap { cartTask => cartTaskInMethodsMap(cartTask) } flatMap groundNew
-          } filter { case (t, _) => scc contains t }
-        }
-      }
     }
 
 
