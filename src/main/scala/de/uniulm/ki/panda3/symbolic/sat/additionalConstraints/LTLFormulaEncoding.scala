84efa87b2e114d06c308e3f9415773e78a78270c
Galvus Damor <gregor.behnke@uni-ulm.de>
2017-11-23 18:03:35 +0100

    removed clauses detecting end of plan

--- src/main/scala/de/uniulm/ki/panda3/symbolic/sat/additionalConstraints/LTLFormulaEncoding.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/sat/additionalConstraints/LTLFormulaEncoding.scala
@@ -15,27 +15,11 @@ case class LTLFormulaEncoding(büchiAutomaton: BüchiAutomaton) extends Addition
 
   private def noPresent(position: Int) = "auto_Present" + position
 
-  private def noAfter(position: Int) = "auto_noAfter" + position
-
-  private def anyAfter(position: Int) = "auto_anyAfter" + position
-
 
   def apply(linearEncoding: EncodingWithLinearPlan): Seq[Clause] = {
     val automatonClauses: Seq[Clause] = linearEncoding.linearPlan.zipWithIndex flatMap { case (taskMap, position) =>
       // at most one of the states of the automaton is true
       val atMostOneState: Seq[Clause] = linearEncoding.atMostOneOf(büchiAutomaton.vertices map { s => state(s, position) })
-
-      // if there is nothing after this index
-      val afterClauses: Seq[Clause] = if (position + 1 == linearEncoding.linearPlan.length) {
-        Clause(noAfter(position)) :: Nil
-      } else {
-        Seq(linearEncoding.impliesSingle(noAfter(position), noAfter(position + 1))) ++ (
-          linearEncoding.linearPlan(position + 1).values map { a => linearEncoding.impliesNot(noAfter(position), a) })
-      }
-
-      // noAfter and anyAfter are inverse of each other
-      val turnAround: Seq[Clause] = linearEncoding.impliesNot(noAfter(position), anyAfter(position)) :: linearEncoding.impliesNot(anyAfter(position), noAfter(position)) ::
-        Clause(Array(noAfter(position), anyAfter(position))) :: Nil
 
       // if a task gets selected, execute the rule
       val transitionRule: Seq[Clause] = büchiAutomaton.vertices flatMap { s =>
@@ -74,7 +58,7 @@ case class LTLFormulaEncoding(büchiAutomaton: BüchiAutomaton) extends Addition
       val noTaskRule: Seq[Clause] = büchiAutomaton.vertices map { s => linearEncoding.impliesRightAndSingle(state(s, position) :: noPresent(position) :: Nil, state(s, position + 1)) }
 
 
-      atMostOneState ++ afterClauses ++ turnAround ++ transitionRule ++ setNoPresent ++ noTaskRule
+      atMostOneState ++ transitionRule ++ setNoPresent ++ noTaskRule
     }
 
     val lastAutomationTransition: Seq[Clause] = {
