1e12a3173bd2d2f9426b44436c452f00fa6c3012
Andrea Biedermann <andrea.biedermann@uni-ulm.de>
2018-01-11 15:48:33 +0100

    Ausmisten

--- src/main/java/de/uniulm/ki/panda3/progression/heuristics/sasp/mergeAndShrink/ShrinkingStrategy.java
+++ src/main/java/de/uniulm/ki/panda3/progression/heuristics/sasp/mergeAndShrink/ShrinkingStrategy.java
@@ -3,14 +3,13 @@ package de.uniulm.ki.panda3.progression.heuristics.sasp.mergeAndShrink;
 import de.uniulm.ki.panda3.progression.htn.representation.SasPlusProblem;
 import de.uniulm.ki.panda3.progression.sasp.mergeAndShrink.NodeValue;
 import de.uniulm.ki.panda3.progression.sasp.mergeAndShrink.ShrinkNode;
-import de.uniulm.ki.util.Dot2PdfCompiler;
 import de.uniulm.ki.util.EdgeLabelledGraph;
+import scala.Tuple2;
 import scala.Tuple3;
 
-import javax.rmi.CORBA.Util;
 import java.util.*;
 
-import static de.uniulm.ki.panda3.progression.heuristics.sasp.mergeAndShrink.ClassicalMergeAndShrink.*;
+//import static de.uniulm.ki.panda3.progression.heuristics.sasp.mergeAndShrink.ClassicalMergeAndShrink.*;
 
 /**
  * Created by biederma on 11.01.2018.
@@ -126,7 +125,7 @@ abstract class ShrinkingStrategy {
 
 
 
-        Set<Integer> goalNodes = getGoalNodes(p, graph);
+        Set<Integer> goalNodes = getGoalNodes(graph);
 
 
         for (int i: goalNodes){
@@ -140,6 +139,43 @@ abstract class ShrinkingStrategy {
 
 
         return distancesToGoalMap;
+
+    }
+
+
+    public static HashMap<Integer, ArrayList<Tuple3<Integer, Integer, Integer>>> getIDToIncomingEdgesMap(EdgeLabelledGraph<Integer, Integer, HashMap<Integer, NodeValue>, Integer, Set<Integer>, Set<Integer>, Set<Integer>, Set<Integer>, CascadingTables> graph){
+
+        HashMap<Integer, ArrayList<Tuple3<Integer, Integer, Integer>>> outgoingEdgesMap = new HashMap<>();
+
+        for (int i: graph.idMapping().keySet()){
+            ArrayList<Tuple3<Integer, Integer, Integer>> edges = new ArrayList<>();
+            outgoingEdgesMap.put(i,edges);
+        }
+
+        for (Tuple3<Integer, Integer, Integer> edge : graph.labelledEdges()){
+
+            outgoingEdgesMap.get(edge._3()).add(edge);
+
+        }
+
+        return outgoingEdgesMap;
+
+    }
+
+    public static Set<Integer> getGoalNodes(EdgeLabelledGraph<Integer, Integer, HashMap<Integer, NodeValue>, Integer, Set<Integer>, Set<Integer>, Set<Integer>, Set<Integer>, CascadingTables> graph){
+
+        HashSet<Integer> goalNodes = new HashSet<>();
+
+        for (int id : graph.idMapping().keySet()){
+
+            NodeValue nodeValue = graph.idMapping().get(id);
+            if (nodeValue.isGoalNode()) goalNodes.add(id);
+
+        }
+
+
+        return goalNodes;
+
 
     }
 
@@ -174,6 +210,211 @@ abstract class ShrinkingStrategy {
         }
 
         return distancesFromGoal;
+
+    }
+
+
+    public static ArrayList<Integer> getNodesFarthestFromStartAndGoal(EdgeLabelledGraph<Integer, Integer, HashMap<Integer, NodeValue>, Integer, Set<Integer>, Set<Integer>, Set<Integer>, Set<Integer>, CascadingTables> graph, HashMap<Integer,Integer> summedDistances){
+
+        ArrayList<Integer> farthestNodes = new ArrayList<>();
+
+        int maximumDistance = Collections.max(summedDistances.values());
+
+        int counter = Collections.frequency(summedDistances.values(),maximumDistance);
+
+        //System.out.println("Maximum: " + maximumDistance + ", Frequency: " + counter);
+
+        if (counter>1) {
+            for (int id : graph.idMapping().keySet()) {
+                if (summedDistances.get(id) == maximumDistance) {
+                    farthestNodes.add(id);
+                }
+            }
+        }else{
+
+           /* ArrayList<Integer> summedDistancesWithoutHighest = new ArrayList<Integer>();
+            summedDistancesWithoutHighest.addAll(summedDistances.values());
+            System.out.println(summedDistancesWithoutHighest);
+            summedDistancesWithoutHighest.remove(maximumDistance);
+            System.out.println(summedDistancesWithoutHighest);*/
+
+
+            HashMap<Integer,Integer> summedDistancesWithoutHighest = new HashMap<>();
+
+            summedDistancesWithoutHighest.putAll(summedDistances);
+
+            //System.out.println(summedDistances);
+            //System.out.println(summedDistancesWithoutHighest);
+
+
+            for (int id : graph.idMapping().keySet()) {
+                if (summedDistances.get(id) == maximumDistance) {
+                    farthestNodes.add(id);
+                    summedDistancesWithoutHighest.remove(id);
+                    break;
+                }
+            }
+
+
+            //System.out.println(summedDistancesWithoutHighest);
+
+
+            int secondMaximumDistance = Collections.max(summedDistancesWithoutHighest.values());
+
+            //System.out.println("Second Maximum: " + secondMaximumDistance);
+
+            for (int id : graph.idMapping().keySet()) {
+                if (summedDistances.get(id) == secondMaximumDistance) {
+                    farthestNodes.add(id);
+                }
+            }
+
+        }
+
+        //System.out.println(farthestNodes);
+
+        return farthestNodes;
+
+    }
+
+
+
+
+
+    public static ArrayList<Integer> getNodesFarthestFromStartAndGoal2(EdgeLabelledGraph<Integer, Integer, HashMap<Integer, NodeValue>, Integer, Set<Integer>, Set<Integer>, Set<Integer>, Set<Integer>, CascadingTables> graph, HashMap<Integer,Integer> summedDistances){
+
+        ArrayList<Integer> farthestNodes = new ArrayList<>();
+
+        int maximumDistance = Collections.max(summedDistances.values());
+
+        int counter = Collections.frequency(summedDistances.values(),maximumDistance);
+
+        //System.out.println("Maximum: " + maximumDistance + ", Frequency: " + counter);
+
+        while(counter<2){
+            /* ArrayList<Integer> summedDistancesWithoutHighest = new ArrayList<Integer>();
+            summedDistancesWithoutHighest.addAll(summedDistances.values());
+            System.out.println(summedDistancesWithoutHighest);
+            summedDistancesWithoutHighest.remove(maximumDistance);
+            System.out.println(summedDistancesWithoutHighest);*/
+
+
+            //System.out.println(summedDistances);
+            //System.out.println(summedDistancesWithoutHighest);
+
+            if (summedDistances.size()<1) break;
+
+
+            for (int id : graph.idMapping().keySet()) {
+                if (summedDistances.get(id) == maximumDistance) {
+                    //farthestNodes.add(id);
+                    summedDistances.remove(id);
+                    break;
+                }
+            }
+
+
+            //System.out.println(summedDistancesWithoutHighest);
+
+
+            maximumDistance = Collections.max(summedDistances.values());
+
+            counter = Collections.frequency(summedDistances.values(),maximumDistance);
+
+            //System.out.println("Second Maximum: " + secondMaximumDistance);
+        }
+
+
+        if (counter>1) {
+            for (int id : graph.idMapping().keySet()) {
+                if (summedDistances.get(id) == maximumDistance) {
+                    farthestNodes.add(id);
+                }
+            }
+        }
+
+        //System.out.println(farthestNodes);
+
+        return farthestNodes;
+
+    }
+
+    public static HashMap<Integer, Integer> getDistancesFromStart(SasPlusProblem p, EdgeLabelledGraph<Integer, Integer, HashMap<Integer, NodeValue>, Integer, Set<Integer>, Set<Integer>, Set<Integer>, Set<Integer>, CascadingTables> graph){
+
+        int startNodeID = graph.startNodeID();
+
+        ArrayList<Integer> nodesToKeep = new ArrayList<>();
+
+        ArrayList<Integer> nextNodes = new ArrayList<>();
+
+        nextNodes.add(startNodeID);
+
+        nodesToKeep.add(startNodeID);
+
+        HashMap<Integer, ArrayList<Tuple3<Integer, Integer, Integer>>> outgoingEdgesMap = getIDToOutgoingEdgesMap(graph);
+
+        HashMap<Integer, Integer> distancesFromStartMap = new HashMap<>();
+
+        distancesFromStartMap.put(graph.startNodeID(), 0);
+
+        Tuple2<ArrayList<Integer>,HashMap<Integer, Integer>> result = breadthSearchToFindDistances(p, nextNodes, nodesToKeep, outgoingEdgesMap, distancesFromStartMap);
+
+        distancesFromStartMap = result._2();
+
+        return distancesFromStartMap;
+    }
+
+    public static HashMap<Integer, ArrayList<Tuple3<Integer, Integer, Integer>>> getIDToOutgoingEdgesMap(EdgeLabelledGraph<Integer, Integer, HashMap<Integer, NodeValue>, Integer, Set<Integer>, Set<Integer>, Set<Integer>, Set<Integer>, CascadingTables> graph){
+
+        HashMap<Integer, ArrayList<Tuple3<Integer, Integer, Integer>>> outgoingEdgesMap = new HashMap<>();
+
+        for (int i: graph.idMapping().keySet()){
+            ArrayList<Tuple3<Integer, Integer, Integer>> edges = new ArrayList<>();
+            outgoingEdgesMap.put(i,edges);
+        }
+
+        for (Tuple3<Integer, Integer, Integer> edge : graph.labelledEdges()){
+
+            outgoingEdgesMap.get(edge._1()).add(edge);
+
+        }
+
+        return outgoingEdgesMap;
+
+    }
+
+    //to find distances from the start node
+    public static Tuple2<ArrayList<Integer>,HashMap<Integer, Integer>> breadthSearchToFindDistances(SasPlusProblem p, ArrayList<Integer> nextNodes, ArrayList<Integer> nodesToKeep,
+                                                                                                    HashMap<Integer, ArrayList<Tuple3<Integer, Integer, Integer>>> outgoingEdgesMap, HashMap<Integer, Integer> distancesFromStart){
+
+        ArrayList<Integer> newNextNodes = new ArrayList<>(nextNodes);
+
+        ArrayList<Integer> reachedNodes = new ArrayList<>(nodesToKeep);
+
+        if (nextNodes.size()>0) {
+
+            int nextNode = nextNodes.get(0);
+
+            int distanceOfNextNodeFromStartNode = distancesFromStart.get(nextNode);
+
+            ArrayList<Tuple3<Integer, Integer, Integer>> outgoingEdges = outgoingEdgesMap.get(nextNode);
+            newNextNodes.remove(0);
+            for (Tuple3<Integer, Integer, Integer> outgoingEdge : outgoingEdges){
+                int endID = outgoingEdge._3();
+                if (!reachedNodes.contains(endID)){
+                    reachedNodes.add(endID);
+                    newNextNodes.add(endID);
+                    int nextDistance = distanceOfNextNodeFromStartNode + p.costs[outgoingEdge._2()];
+                    //distanceOfNextNodeFromStartNode + 1;
+                    distancesFromStart.put(endID, nextDistance);
+                }
+            }
+
+            return breadthSearchToFindDistances(p, newNextNodes, reachedNodes, outgoingEdgesMap, distancesFromStart);
+
+        }
+
+        return new Tuple2<>(reachedNodes, distancesFromStart);
 
     }
 
@@ -227,7 +468,10 @@ class ShrinkingStrategy1 extends ShrinkingStrategy{
 
 
 
+
 }
+
+
 
 
 class ShrinkingStrategy2 extends ShrinkingStrategy {
