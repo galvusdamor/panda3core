9956317409a5700183da289249af1593699aa0d1
Galvus Damor <gregor.behnke@uni-ulm.de>
2018-02-23 21:20:29 +0100

    made predicate pruning more efficient by using cached data structures

--- src/main/scala/de/uniulm/ki/panda3/symbolic/compiler/pruning/PrunePredicates.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/compiler/pruning/PrunePredicates.scala
@@ -13,16 +13,12 @@ object PrunePredicates extends DomainTransformer[Set[String]] {
   override def transform(domain: Domain, plan: Plan, predicatesToKeep: Set[String]): (Domain, Plan) = {
     val unnecessaryPredicates = domain.predicates filter { p =>
       // it might be true and cannot be made false
-      if ((plan.groundInitialStateOnlyPositivesSetOnlyPredicates contains p) && !(domain.tasks exists { _.effectsAsPredicateBool.contains((p, false)) })) {
+      if ((plan.groundInitialStateOnlyPositivesSetOnlyPredicates contains p) && domain.producersOfPosNeg(p)._2.isEmpty)
         true
-      } else if (!(plan.groundInitialStateOnlyPositivesSetOnlyPredicates contains p) &&
-        !(domain.tasks exists { _.effectsAsPredicateBool.contains((p, true)) }) &&
-        !(domain.tasks exists { _.preconditionsAsPredicateBool.contains((p, true)) })
-      ) {
+      else if (!(plan.groundInitialStateOnlyPositivesSetOnlyPredicates contains p) && domain.consumersOf(p).isEmpty && domain.producersOfPosNeg(p)._1.isEmpty)
         true
-      } else {
+      else
         false
-      }
     } filterNot { p => predicatesToKeep contains p.name.split("\\[").head }
 
     (domain update RemovePredicate(unnecessaryPredicates.toSet), plan update RemovePredicate(unnecessaryPredicates.toSet))
