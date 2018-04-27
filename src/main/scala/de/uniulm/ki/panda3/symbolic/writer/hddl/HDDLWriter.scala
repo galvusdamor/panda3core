5158c7d1153b3ceeb27a1ffe1926a95800ac4f2b
Galvus Damor <gregor.behnke@uni-ulm.de>
2018-04-27 11:42:05 +0200

    bugfix and added copy right

--- src/main/scala/de/uniulm/ki/panda3/symbolic/writer/hddl/HDDLWriter.scala
+++ src/main/scala/de/uniulm/ki/panda3/symbolic/writer/hddl/HDDLWriter.scala
@@ -1,3 +1,19 @@
+// PANDA 3 -- a domain-independent planner for classical and hierarchical planning
+// Copyright (C) 2014-2017 the original author or authors.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package de.uniulm.ki.panda3.symbolic.writer.hddl
 
 import de.uniulm.ki.panda3.symbolic.csp._
@@ -349,7 +365,9 @@ case class HDDLWriter(domainName: String, problemName: String) extends Writer {
           builder.append("\t\t:precondition (and\n")
           if (neededVariableConstraints.nonEmpty)
             neededVariableConstraints foreach {
-              case (v, c) => builder.append("\t\t\t(= " + toHPDDLVariableName(taskUF.getRepresentative(v).asInstanceOf[Variable].name) + " " + toPDDLIdentifier(c.name) + ")\n")
+              case (v, c) => builder.append("\t\t\t(= " +
+                                              (if (noConstantReplacement) toHPDDLVariableName(v.name) else toHPDDLVariableName(taskUF.getRepresentative(v).asInstanceOf[Variable].name)) + " " +
+                                                toPDDLIdentifier(c.name) + ")\n")
             }
           if (!methodPrecondition.isEmpty) writeFormula(builder, methodPrecondition, "\t\t\t", planUF, noConstantReplacement)
           builder.append("\t\t)\n")
